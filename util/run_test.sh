d4b754a8b1ebbccb0a67df0e966e5b670850e60f
Wang Xiaozhe <chaoslawful@gmail.com>
2010-05-18 18:35:05 +0800

    added basic tests and reindex script

--- util/run_test.sh
+++ util/run_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 script_dir=$(dirname $0)
 root=$(readlink -f $script_dir/..)
+cd $root
+git submodule update --init
+$script_dir/reindex $root/t/*.t
 export PATH=$root/work/sbin:$PATH
 killall nginx
 prove -I$root/t/test-nginx/lib $root/t/*.t
