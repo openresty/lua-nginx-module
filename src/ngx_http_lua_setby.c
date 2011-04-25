0e0b0fc267581ddc7a5e70850074928bfd8f33e8
Wang Xiaozhe <chaoslawful@gmail.com>
2011-04-25 16:01:02 +0800

    also apply PCRE patch to set_by_lua* directives

--- src/ngx_http_lua_setby.c
+++ src/ngx_http_lua_setby.c
@@ -1,8 +1,10 @@
 /* vim:set ft=c ts=4 sw=4 et fdm=marker: */
 #define DDEBUG 0
+
 #include "ngx_http_lua_setby.h"
 #include "ngx_http_lua_hook.h"
 #include "ngx_http_lua_util.h"
+#include "ngx_http_lua_patch.h"
 
 
 static int
@@ -188,8 +190,15 @@ ngx_http_lua_set_by_chunk(lua_State *L, ngx_http_request_t *r, ngx_str_t *val,
         lua_pushlstring(L, (const char *) args[i].data, args[i].len);
     }
 
+    // XXX: work-around to nginx regex subsystem
+    ngx_http_lua_pcre_malloc_init(r->pool);
+
     /*  protected call user code */
     rc = lua_pcall(L, nargs, 1, 0);
+
+    // XXX: work-around to nginx regex subsystem
+    ngx_http_lua_pcre_malloc_done();
+
     if (rc) {
         /*  error occured when running loaded code */
         const char *err_msg = lua_tostring(L, -1);
