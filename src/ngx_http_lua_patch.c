cfa8109d9723d71f8a5f50a37ac4790bb04c4065
agentzh (章亦春) <agentzh@gmail.com>
2011-05-09 23:41:06 +0800

    minor coding style fixes.

--- src/ngx_http_lua_patch.c
+++ src/ngx_http_lua_patch.c
@@ -1,10 +1,12 @@
-#include "nginx.h"
+#include <nginx.h>
 #include "ngx_http_lua_patch.h"
+
 
 static ngx_pool_t *ngx_http_lua_pcre_pool;
 
-static void* (*old_pcre_malloc)(size_t);
+static void *(*old_pcre_malloc)(size_t);
 static void (*old_pcre_free)(void *ptr);
+
 
 /* XXX: work-around to nginx regex subsystem, must init a memory pool
  * to use PCRE functions. As PCRE still has memory-leaking problems,
@@ -13,34 +15,41 @@ static void (*old_pcre_free)(void *ptr);
 static void *
 ngx_http_lua_pcre_malloc(size_t size)
 {
-	if(ngx_http_lua_pcre_pool) {
+	if (ngx_http_lua_pcre_pool) {
 		return ngx_palloc(ngx_http_lua_pcre_pool, size);
 	}
+
 	return NULL;
 }
+
 
 static void
 ngx_http_lua_pcre_free(void *ptr)
 {
-	if(ngx_http_lua_pcre_pool) {
+	if (ngx_http_lua_pcre_pool) {
 		ngx_pfree(ngx_http_lua_pcre_pool, ptr);
 	}
 }
+
 
 void
 ngx_http_lua_pcre_malloc_init(ngx_pool_t *pool)
 {
 	ngx_http_lua_pcre_pool = pool;
+
 	old_pcre_malloc = pcre_malloc;
 	old_pcre_free = pcre_free;
+
     pcre_malloc = ngx_http_lua_pcre_malloc;
     pcre_free = ngx_http_lua_pcre_free;
 }
+
 
 void
 ngx_http_lua_pcre_malloc_done()
 {
 	ngx_http_lua_pcre_pool = NULL;
+
 	pcre_malloc = old_pcre_malloc;
 	pcre_free = old_pcre_free;
 }
--- src/ngx_http_lua_patch.h
+++ src/ngx_http_lua_patch.h
@@ -1,10 +1,13 @@
 #ifndef NGX_HTTP_LUA_PATCH_H
 #define NGX_HTTP_LUA_PATCH_H
 
+
 #include "ngx_http_lua_common.h"
 
-extern void ngx_http_lua_pcre_malloc_init(ngx_pool_t *pool);
-extern void ngx_http_lua_pcre_malloc_done();
+
+void ngx_http_lua_pcre_malloc_init(ngx_pool_t *pool);
+void ngx_http_lua_pcre_malloc_done();
+
 
 #endif /* NGX_HTTP_LUA_PATCH_H */
 /* vim:set ft=c ts=4 sw=4 et fdm=marker: */
