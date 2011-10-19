982cb581a93c665e7766f96eb8954d6b0e3a8582
agentzh (章亦春) <agentzh@gmail.com>
2011-10-19 12:23:18 +0800

    refactored the code for access_by_lua and access_by_lua_file to reduce code duplication.

--- src/ngx_http_lua_accessby.h
+++ src/ngx_http_lua_accessby.h
@@ -7,7 +7,6 @@
 #include "ngx_http_lua_common.h"
 
 
-ngx_int_t ngx_http_lua_access_by_chunk(lua_State *L, ngx_http_request_t *r);
 ngx_int_t ngx_http_lua_access_handler(ngx_http_request_t *r);
 ngx_int_t ngx_http_lua_access_handler_inline(ngx_http_request_t *r);
 ngx_int_t ngx_http_lua_access_handler_file(ngx_http_request_t *r);
