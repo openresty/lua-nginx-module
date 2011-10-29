dc49eb2067bb5495133007cbf020c7b96b51b303
agentzh (章亦春) <agentzh@gmail.com>
2011-10-29 16:17:01 +0800

    feature: added "ctx" option to ngx.location.capture: you can now specify a custom Lua table to pass to the subrequest as its "ngx.ctx". thanks @hugozhu.

--- src/ngx_http_lua_ctx.h
+++ src/ngx_http_lua_ctx.h
@@ -7,6 +7,8 @@
 
 int ngx_http_lua_ngx_get_ctx(lua_State *L);
 int ngx_http_lua_ngx_set_ctx(lua_State *L);
+int ngx_http_lua_ngx_set_ctx_helper(lua_State *L, ngx_http_request_t *r,
+        ngx_http_lua_ctx_t *ctx, int index);
 
 
 #endif /* NGX_HTTP_LUA_CTX_H */
