b8e5b0219416efec304d0ed71b6327309921cbec
agentzh (章亦春) <agentzh@gmail.com>
2012-06-10 21:39:28 +0800

    fixed the "ngx" table initial size for set_by_lua*.

--- src/ngx_http_lua_setby.c
+++ src/ngx_http_lua_setby.c
@@ -269,7 +269,7 @@ ngx_http_lua_inject_setby_ngx_api(ngx_conf_t *cf, lua_State *L)
     lmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_lua_module);
 
     lua_pushlightuserdata(L, &ngx_http_lua_setby_ngx_key);
-    lua_createtable(L, 0 /* narr */, 72 /* nrec */);    /*  ngx.* */
+    lua_createtable(L, 0 /* narr */, 69 /* nrec */);    /*  ngx.* */
 
     ngx_http_lua_inject_core_consts(L);
     ngx_http_lua_inject_http_consts(L);
