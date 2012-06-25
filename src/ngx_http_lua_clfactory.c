47eff35e911c4952435ccf3333f3f51a9c31a53f
Wang Xiaozhe <chaoslawful@gmail.com>
2012-06-25 14:24:43 +0800

    bugfix: fix issue #132 dereferencing type-punned pointer will break strict-aliasing rules

--- src/ngx_http_lua_clfactory.c
+++ src/ngx_http_lua_clfactory.c
@@ -466,12 +466,12 @@ ngx_http_lua_clfactory_bytecode_prepare(lua_State *L, clfactory_file_ctx_t *lf,
         }
 #endif
 
-        /* lineinfo vector size */
-        *(int *) (lf->end_code.str) = 0;
-        /* number of local vars */
-        *(int *) (lf->end_code.str + sizeof(int)) = 0;
-        /* number of upvalues */
-        *(int *) (lf->end_code.str + sizeof(int) + sizeof(int)) = 0;
+        /* set the following fields to zero:
+         * - lineinfo vector size
+         * - number of local vars
+         * - number of upvalues
+         */
+        memset(lf->end_code.str, 0, sizeof(int) * 3);
 
         lf->end_code_len = sizeof(int) + sizeof(int) + sizeof(int);
 
