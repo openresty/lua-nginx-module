c314499de7e862b4f6cbd8497e86814171ef3fef
agentzh (章亦春) <agentzh@gmail.com>
2012-08-08 12:19:26 -0700

    fixed a source line exceeding 80 cols.

--- src/ngx_http_lua_util.c
+++ src/ngx_http_lua_util.c
@@ -914,8 +914,8 @@ ngx_http_lua_run_thread(lua_State *L, ngx_http_request_t *r,
                  */
                 switch(ctx->cc_op) {
                 case RESUME:
-                    ngx_log_debug0(NGX_LOG_DEBUG_HTTP,
-                                   r->connection->log, 0, "lua coroutine: resume");
+                    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                                   "lua coroutine: resume");
 
                     /*
                      * the target coroutine lies at the base of the
