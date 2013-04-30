cea321f7f9e2614c9e0932e367d15502507fa7e9
agentzh (Yichun Zhang) <agentzh@gmail.com>
2013-04-30 16:46:12 -0700

    added pure C API for ngx.md5, ngx.md5_bin, ngx.sha1_bin, which is expected to be used with LuaJIT FFI (or lua-resty-core in particular). such API can be excluded by specifying the C macro NGX_HTTP_LUA_NO_FFI_API.

--- src/ngx_http_lua_string.c
+++ src/ngx_http_lua_string.c
@@ -667,4 +667,43 @@ ngx_http_lua_ngx_hmac_sha1(lua_State *L)
 }
 #endif
 
+
+#ifndef NGX_HTTP_LUA_NO_FFI_API
+void
+ngx_http_lua_ffi_md5_bin(const u_char *src, size_t len, u_char *dst)
+{
+    ngx_md5_t     md5;
+
+    ngx_md5_init(&md5);
+    ngx_md5_update(&md5, src, len);
+    ngx_md5_final(dst, &md5);
+}
+
+
+void
+ngx_http_lua_ffi_md5(const u_char *src, size_t len, u_char *dst)
+{
+    ngx_md5_t           md5;
+    u_char              md5_buf[MD5_DIGEST_LENGTH];
+
+    ngx_md5_init(&md5);
+    ngx_md5_update(&md5, src, len);
+    ngx_md5_final(md5_buf, &md5);
+
+    ngx_hex_dump(dst, md5_buf, sizeof(md5_buf));
+}
+
+
+void
+ngx_http_lua_ffi_sha1_bin(const u_char *src, size_t len, u_char *dst)
+{
+    ngx_sha1_t               sha;
+
+    ngx_sha1_init(&sha);
+    ngx_sha1_update(&sha, src, len);
+    ngx_sha1_final(dst, &sha);
+}
+
+#endif
+
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */
