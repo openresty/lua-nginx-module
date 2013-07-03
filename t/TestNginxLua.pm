d82196f7fd0f074335ea3434826c303383a5e0f8
Yichun Zhang (agentzh) <agentzh@gmail.com>
2013-07-02 17:11:07 -0700

    bugfix: t::TestNginxLua: env TEST_NGINX_INIT_BY_LUA would break existing init_by_lua_file directive in the test case.

--- t/TestNginxLua.pm
+++ t/TestNginxLua.pm
@@ -8,6 +8,9 @@ if ($code) {
 
     Test::Nginx::Socket::set_http_config_filter(sub {
         my $config = shift;
+        if ($config =~ /init_by_lua_file/) {
+            return $config;
+        }
         unless ($config =~ s{init_by_lua\s*(['"])((?:\\.|.)*)\1\s*;}{init_by_lua $1$code$2$1;}s) {
             $config .= "init_by_lua '$code';";
         }
