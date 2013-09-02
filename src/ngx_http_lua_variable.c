db7a8c90c16e1a1b42d0a9bf9c4e0a337629cfae
Yichun Zhang (agentzh) <agentzh@gmail.com>
2013-09-01 22:44:00 -0700

    feature: added new FFI C API function ngx_http_lua_ffi_var_get for reading ngx.var.VARIABLE.

--- src/ngx_http_lua_variable.c
+++ src/ngx_http_lua_variable.c
@@ -277,4 +277,80 @@ ngx_http_lua_var_set(lua_State *L)
                       lowcase, lowcase);
 }
 
+
+#ifndef NGX_HTTP_LUA_NO_FFI_API
+int
+ngx_http_lua_ffi_var_get(ngx_http_request_t *r, u_char *name_data,
+    size_t name_len, u_char *lowcase_buf, int capture_id, u_char **value,
+    size_t *value_len, char **err)
+{
+    u_char                      *p;
+    ngx_uint_t                   hash;
+    ngx_str_t                    name;
+    ngx_http_variable_value_t   *vv;
+
+#if (NGX_PCRE)
+    ngx_uint_t                   n;
+    int                         *cap;
+#endif
+
+    if (r == NULL) {
+        *err = "no request object found";
+        return NGX_ERROR;
+    }
+
+    if ((r)->connection->fd == -1) {
+        *err = "API disabled in the current context";
+        return NGX_ERROR;
+    }
+
+#if (NGX_PCRE)
+    if (name_data == 0) {
+        if (capture_id <= 0) {
+            return NGX_DECLINED;
+        }
+
+        /* it is a regex capturing variable */
+
+        n = (ngx_uint_t) capture_id * 2;
+
+        dd("n = %d, ncaptures = %d", (int) n, (int) r->ncaptures);
+
+        if (r->captures == NULL
+            || r->captures_data == NULL
+            || n >= r->ncaptures)
+        {
+            return NGX_DECLINED;
+        }
+
+        /* n >= 0 && n < r->ncaptures */
+
+        cap = r->captures;
+        p = r->captures_data;
+
+        *value = &p[cap[n]];
+        *value_len = (size_t) (cap[n + 1] - cap[n]);
+
+        return NGX_OK;
+    }
+#endif
+
+    hash = ngx_hash_strlow(lowcase_buf, name_data, name_len);
+
+    name.data = lowcase_buf;
+    name.len = name_len;
+
+    dd("variable name: %.*s", (int) name_len, lowcase_buf);
+
+    vv = ngx_http_get_variable(r, &name, hash);
+    if (vv == NULL || vv->not_found) {
+        return NGX_DECLINED;
+    }
+
+    *value = vv->data;
+    *value_len = vv->len;
+    return NGX_OK;
+}
+#endif /* NGX_HTTP_LUA_NO_FFI_API */
+
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */
