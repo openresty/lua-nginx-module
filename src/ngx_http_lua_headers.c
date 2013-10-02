openresty/lua-nginx-module

commit d8a266497a7078de0b5ecbc71da07917462af335

optimize: now we pre-calculate the exact size of the resulting Lua table and preallocate the table space, which makes it 8%+ faster for a request with a dozen request headers and 40%+ faster for a hundred request headers.

src/ngx_http_lua_headers.c
@@ -322,7 +322,18 @@ ngx_http_lua_ngx_req_get_headers(lua_State *L)
 
     ngx_http_lua_check_fake_request(L, r);
 
-    lua_createtable(L, 0, 4);
+    part = &r->headers_in.headers.part;
+    count = part->nelts;
+    while (part->next) {
+        part = part->next;
+        count += part->nelts;
+    }
+
+    if (max > 0 && count > max) {
+        count = max;
+    }
+
+    lua_createtable(L, 0, count);
 
     if (!raw) {
         lua_pushlightuserdata(L, &ngx_http_lua_req_get_headers_metatable_key);
@@ -366,7 +377,7 @@ ngx_http_lua_ngx_req_get_headers(lua_State *L)
                        "lua request header: \"%V: %V\"",
                        &header[i].key, &header[i].value);
 
-        if (max > 0 && ++count == max) {
+        if (--count == 0) {
             ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
                            "lua hit request header limit %d", max);
 

commit e5d47c4488742787ac38be64cd45c2ae91cd898e

bugfix: we always printed the "lua hit request header limit" debug log message even when the limit is not hit at all. this regression had appeared in commit d8a266497a.

src/ngx_http_lua_headers.c
@@ -331,6 +331,8 @@ ngx_http_lua_ngx_req_get_headers(lua_State *L)
 
     if (max > 0 && count > max) {
         count = max;
+        ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                       "lua exceeding request header limit %d", max);
     }
 
     lua_createtable(L, 0, count);
@@ -378,9 +380,6 @@ ngx_http_lua_ngx_req_get_headers(lua_State *L)
                        &header[i].key, &header[i].value);
 
         if (--count == 0) {
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                           "lua hit request header limit %d", max);
-
             return 1;
         }
     }

t/028-req-header.t
@@ -9,7 +9,7 @@ use t::TestNginxLua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (2 * blocks() + 18);
+plan tests => repeat_each() * (2 * blocks() + 19);
 
 #no_diff();
 #no_long_string();
@@ -34,6 +34,9 @@ Bar: baz
 --- response_body
 Foo: bar
 Bar: baz
+--- log_level: debug
+--- no_error_log
+lua exceeding request header limit
 
 
 
@@ -427,7 +430,7 @@ for my $k (@k) {
 CORE::join("", @k);
 --- timeout: 4
 --- error_log
-lua hit request header limit 100
+lua exceeding request header limit 100
 
 
 
@@ -475,7 +478,7 @@ for my $k (@k) {
 CORE::join("", @k);
 --- timeout: 4
 --- error_log
-lua hit request header limit 102
+lua exceeding request header limit 102
 
 
 