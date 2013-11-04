3c0a9d90bd60d38107bdf40fb120ad1157625f43
Yichun Zhang (agentzh) <agentzh@gmail.com>
2013-11-04 13:07:48 -0800

    add debugging logs for the Lua VM initialization and shutdown.

--- src/ngx_http_lua_module.c
+++ src/ngx_http_lua_module.c
@@ -738,9 +738,8 @@ ngx_http_lua_cleanup_vm(void *data)
     lua_State *L = data;
 
     if (L != NULL) {
+        ngx_log_debug0(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0, "lua close the global Lua VM");
         lua_close(L);
-
-        dd("Lua VM closed!");
     }
 }
 
@@ -752,6 +751,8 @@ ngx_http_lua_init_vm(ngx_conf_t *cf, ngx_http_lua_main_conf_t *lmcf)
     ngx_http_lua_preload_hook_t     *hook;
     lua_State                       *L;
     ngx_uint_t                       i;
+
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->log, 0, "lua initialize the global Lua VM");
 
     ngx_http_lua_content_length_hash =
         ngx_http_lua_hash_literal("content-length");
