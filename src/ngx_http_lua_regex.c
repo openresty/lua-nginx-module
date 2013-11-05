279da4d09039aba65c0c6ab7812fd9a8f0c497b8
Yichun Zhang (agentzh) <agentzh@gmail.com>
2013-11-04 16:56:11 -0800

    feature: added new API function ngx.re.find, which is similar to ngx.re.match, but only returns the beginning index and end index of the whole match.

--- src/ngx_http_lua_regex.c
+++ src/ngx_http_lua_regex.c
@@ -80,6 +80,8 @@ static int ngx_http_lua_ngx_re_gmatch_iterator(lua_State *L);
 static ngx_uint_t ngx_http_lua_ngx_re_parse_opts(lua_State *L,
     ngx_http_lua_regex_compile_t *re, ngx_str_t *opts, int narg);
 static int ngx_http_lua_ngx_re_sub_helper(lua_State *L, unsigned global);
+static int ngx_http_lua_ngx_re_match_helper(lua_State *L, int wantcaps);
+static int ngx_http_lua_ngx_re_find(lua_State *L);
 static int ngx_http_lua_ngx_re_match(lua_State *L);
 static int ngx_http_lua_ngx_re_gmatch(lua_State *L);
 static int ngx_http_lua_ngx_re_sub(lua_State *L);
@@ -107,6 +109,20 @@ static void ngx_http_lua_re_collect_named_captures(lua_State *L,
 
 static int
 ngx_http_lua_ngx_re_match(lua_State *L)
+{
+    return ngx_http_lua_ngx_re_match_helper(L, 1 /* want captures */);
+}
+
+
+static int
+ngx_http_lua_ngx_re_find(lua_State *L)
+{
+    return ngx_http_lua_ngx_re_match_helper(L, 0 /* want captures */);
+}
+
+
+static int
+ngx_http_lua_ngx_re_match_helper(lua_State *L, int wantcaps)
 {
     /* u_char                      *p; */
     ngx_http_request_t          *r;
@@ -288,8 +304,11 @@ ngx_http_lua_ngx_re_match(lua_State *L)
         dd("compile failed");
 
         lua_pushnil(L);
+        if (!wantcaps) {
+            lua_pushnil(L);
+        }
         lua_pushlstring(L, (char *) re_comp.err.data, re_comp.err.len);
-        return 2;
+        return wantcaps ? 2 : 3;
     }
 
 #if (LUA_HAVE_PCRE_JIT)
@@ -475,7 +494,10 @@ exec:
         }
 
         lua_pushnil(L);
-        return 1;
+        if (!wantcaps) {
+            lua_pushnil(L);
+        }
+        return wantcaps ? 1 : 2;
     }
 
     if (rc < 0) {
@@ -494,6 +516,18 @@ exec:
     }
 
     dd("rc = %d", (int) rc);
+
+    if (nargs == 4) { /* having ctx table */
+        pos = cap[1];
+        lua_pushinteger(L, (lua_Integer) (pos + 1));
+        lua_setfield(L, 4, "pos");
+    }
+
+    if (!wantcaps) {
+        lua_pushinteger(L, cap[0] + 1);
+        lua_pushinteger(L, cap[1]);
+        return 2;
+    }
 
     lua_createtable(L, rc /* narr */, 0 /* nrec */);
 
@@ -515,12 +549,6 @@ exec:
     if (name_count > 0) {
         ngx_http_lua_re_collect_named_captures(L, name_table, name_count,
                                                name_entry_size, flags, &subj);
-    }
-
-    if (nargs == 4) { /* having ctx table */
-        pos = cap[1];
-        lua_pushinteger(L, (lua_Integer) (pos + 1));
-        lua_setfield(L, 4, "pos");
     }
 
     if (!(flags & NGX_LUA_RE_COMPILE_ONCE)) {
@@ -551,8 +579,11 @@ error:
     }
 
     lua_pushnil(L);
+    if (!wantcaps) {
+        lua_pushnil(L);
+    }
     lua_pushstring(L, msg);
-    return 2;
+    return wantcaps ? 2 : 3;
 }
 
 
@@ -1810,7 +1841,10 @@ ngx_http_lua_inject_regex_api(lua_State *L)
 {
     /* ngx.re */
 
-    lua_createtable(L, 0, 4 /* nrec */);    /* .re */
+    lua_createtable(L, 0, 5 /* nrec */);    /* .re */
+
+    lua_pushcfunction(L, ngx_http_lua_ngx_re_find);
+    lua_setfield(L, -2, "find");
 
     lua_pushcfunction(L, ngx_http_lua_ngx_re_match);
     lua_setfield(L, -2, "match");
