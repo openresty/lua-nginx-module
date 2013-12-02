openresty/lua-nginx-module

commit 5fc80b5b0eee31399e23e4a3e0ceb2e33c38cc9f

bugfix: turning off lua_code_cache could lead to memory issues (segfaults and LuaJIT assertion failures) when Lua libraries using LuaJIT FFI were loaded. now we always create clean separate lua VM instances for every Nginx request when the Lua code cache is disabled. thanks Ron Gomes for the report.

src/ngx_http_lua_util.c
@@ -118,6 +118,8 @@ static ngx_int_t ngx_http_lua_flush_pending_output(ngx_http_request_t *r,
 static ngx_int_t
     ngx_http_lua_process_flushing_coroutines(ngx_http_request_t *r,
     ngx_http_lua_ctx_t *ctx);
+static lua_State * ngx_http_lua_new_state(ngx_cycle_t *cycle,
+    ngx_http_lua_main_conf_t *lmcf, ngx_log_t *log);
 
 
 #ifndef LUA_PATH_SEP
@@ -183,7 +185,7 @@ ngx_http_lua_create_new_global_table(lua_State *L, int narr, int nrec)
 }
 
 
-lua_State *
+static lua_State *
 ngx_http_lua_new_state(ngx_cycle_t *cycle, ngx_http_lua_main_conf_t *lmcf,
     ngx_log_t *log)
 {
@@ -974,7 +976,14 @@ ngx_http_lua_request_cleanup(ngx_http_lua_ctx_t *ctx, int forcible)
     }
 #endif
 
-    L = ngx_http_lua_get_main_lua_state(r);
+    cur_ctx = ngx_http_get_module_ctx(r, ngx_http_lua_module);
+    if (cur_ctx != ctx && ctx->vm_cleanup_data) {
+        /* internal redirect happens and code cache is off */
+        L = ctx->vm_cleanup_data->state;
+
+    } else {
+        L = ngx_http_lua_get_main_lua_state(r);
+    }
 
     /* we cannot release the ngx.ctx table if we have log_by_lua* hooks
      * because request cleanup runs before log phase handlers */
@@ -985,8 +994,6 @@ ngx_http_lua_request_cleanup(ngx_http_lua_ctx_t *ctx, int forcible)
             ngx_http_lua_release_ngx_ctx_table(r->connection->log, L, ctx);
 
         } else {
-
-            cur_ctx = ngx_http_get_module_ctx(r, ngx_http_lua_module);
 
             if (cur_ctx != ctx) {
                 /* internal redirects happened */
@@ -3602,6 +3609,94 @@ ngx_http_lua_close_fake_connection(ngx_connection_t *c)
 
     if (pool) {
         ngx_destroy_pool(pool);
+    }
+}
+
+
+lua_State *
+ngx_http_lua_init_vm(ngx_cycle_t *cycle, ngx_pool_t *pool,
+    ngx_http_lua_main_conf_t *lmcf, ngx_log_t *log,
+    ngx_pool_cleanup_t **pcln)
+{
+    lua_State                       *L;
+    ngx_uint_t                       i;
+    ngx_pool_cleanup_t              *cln;
+    ngx_http_lua_preload_hook_t     *hook;
+    ngx_http_lua_vm_cleanup_data_t  *data;
+
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, log, 0, "lua initialize the "
+                   "global Lua VM");
+
+    /* add new cleanup handler to config mem pool */
+    cln = ngx_pool_cleanup_add(pool, sizeof(ngx_http_lua_vm_cleanup_data_t));
+    if (cln == NULL) {
+        return NULL;
+    }
+
+    /* create new Lua VM instance */
+    L = ngx_http_lua_new_state(cycle, lmcf, log);
+    if (L == NULL) {
+        return NULL;
+    }
+
+    /* register cleanup handler for Lua VM */
+    cln->handler = ngx_http_lua_cleanup_vm;
+
+    data = ngx_alloc(sizeof(ngx_http_lua_vm_cleanup_data_t), log);
+    if (data == NULL) {
+        return NULL;
+    }
+    data->state = L;
+    data->count = 1;
+
+    cln->data = data;
+
+    if (pcln) {
+        *pcln = cln;
+    }
+
+    if (lmcf->preload_hooks) {
+
+        /* register the 3rd-party module's preload hooks */
+
+        lua_getglobal(L, "package");
+        lua_getfield(L, -1, "preload");
+
+        hook = lmcf->preload_hooks->elts;
+
+        for (i = 0; i < lmcf->preload_hooks->nelts; i++) {
+
+            ngx_http_lua_probe_register_preload_package(L, hook[i].package);
+
+            lua_pushcfunction(L, hook[i].loader);
+            lua_setfield(L, -2, (char *) hook[i].package);
+        }
+
+        lua_pop(L, 2);
+    }
+
+    return L;
+}
+
+
+void
+ngx_http_lua_cleanup_vm(void *data)
+{
+    lua_State                       *L;
+    ngx_http_lua_vm_cleanup_data_t  *cln_data = data;
+
+#if (DDEBUG)
+    if (cln_data) {
+        dd("cleanup VM: c:%d, s:%p", (int) cln_data->count, cln_data->state);
+    }
+#endif
+
+    if (cln_data && --cln_data->count == 0) {
+        L = cln_data->state;
+        ngx_log_debug0(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0, "lua close the "
+                       "global Lua VM");
+        lua_close(L);
+        ngx_free(cln_data);
     }
 }
 

t/025-codecache.t
@@ -5,7 +5,7 @@ use t::TestNginxLua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 3 + 1);
+plan tests => repeat_each() * 108;
 
 #$ENV{LUA_PATH} = $ENV{HOME} . '/work/JSON4Lua-0.9.30/json/?.lua';
 
@@ -501,7 +501,7 @@ qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
 
 
 
-=== TEST 15: skip luarocks
+=== TEST 15: do not skip luarocks
 --- http_config eval
     "lua_package_path '$::HtmlDir/?.lua;./?.lua';
      lua_code_cache off;"
@@ -544,14 +544,14 @@ end;
 --- response_body
 loading
 hello, foo
-found
-found
+not found
+not found
 --- error_log eval
 qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
 
 
 
-=== TEST 16: skip luarocks*
+=== TEST 16: do not skip luarocks*
 --- http_config eval
     "lua_package_path '$::HtmlDir/?.lua;./?.lua';
      lua_code_cache off;"
@@ -594,8 +594,8 @@ end;
 --- response_body
 loading
 hello, foo
-found
-found
+not found
+not found
 --- error_log eval
 qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
 
@@ -652,4 +652,349 @@ GET /t
 --- error_log eval
 qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
 
+
+
+=== TEST 19: lua_code_cache off + FFI-based Lua modules
+--- http_config
+    lua_code_cache off;
+    lua_package_path "$prefix/html/?.lua;;";
+
+--- config
+    location = /t {
+        content_by_lua '
+            if not jit then
+                ngx.say("skipped for non-LuaJIT")
+            else
+                local test = require("test")
+                ngx.say("test module loaded: ", test and true or false)
+                collectgarbage()
+            end
+        ';
+    }
+--- user_files
+>>> test.lua
+local ffi = require "ffi"
+
+ffi.cdef[[
+    int my_test_function_here(void *p);
+    int my_test_function_here2(void *p);
+    int my_test_function_here3(void *p);
+]]
+
+return {
+}
+--- request
+GET /t
+--- response_body_like chop
+^(?:skipped for non-LuaJIT|test module loaded: true)$
+--- no_error_log
+[error]
+--- error_log eval
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
+
+
+
+=== TEST 20: ngx.timer.* + ndk
+--- config
+    lua_code_cache off;
+    location /read {
+        echo ok;
+        log_by_lua '
+            ngx.timer.at(0, function ()
+                local foo = ndk.set_var.set_unescape_uri("a%20b")
+                ngx.log(ngx.WARN, "foo = ", foo)
+            end)
+        ';
+    }
+--- request
+GET /read
+--- response_body
+ok
+--- wait: 0.1
+--- no_error_log
+[error]
+--- error_log eval
+["foo = a b",
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
+]
+
+
+
+=== TEST 21: set ngx.ctx before internal redirects performed by other nginx modules (with log_by_lua)
+--- config
+    lua_code_cache off;
+    location = /t {
+        rewrite_by_lua '
+            ngx.ctx.foo = "hello world";
+        ';
+        echo_exec /foo;
+    }
+
+    location = /foo {
+        echo hello;
+        log_by_lua return;
+    }
+--- request
+GET /t
+--- response_body
+hello
+--- no_error_log
+[error]
+--- log_level: debug
+--- error_log eval
+["lua release ngx.ctx at ref",
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
+"lua close the global Lua VM",
+]
+
+
+
+=== TEST 22: set by lua file
+--- config
+    lua_code_cache off;
+    location /lua {
+        set_by_lua_file $res html/a.lua $arg_a $arg_b;
+        echo $res;
+    }
+--- user_files
+>>> a.lua
+return ngx.arg[1] + ngx.arg[2]
+--- request
+GET /lua?a=5&b=2
+--- response_body
+7
+--- no_error_log
+[error]
+--- error_log eval
+[qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
+"lua close the global Lua VM",
+]
+
+
+
+=== TEST 23: simple set by lua
+--- config
+    lua_code_cache off;
+    location /lua {
+        set_by_lua $res "return 1+1";
+        echo $res;
+    }
+--- request
+GET /lua
+--- response_body
+2
+--- no_error_log
+[error]
+--- error_log eval
+[
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
+"lua close the global Lua VM",
+]
+
+
+
+=== TEST 24: lua_max_pending_timers - chained timers (non-zero delay) - not exceeding
+--- http_config
+    lua_max_pending_timers 1;
+    lua_code_cache off;
+
+--- config
+    location /t {
+        content_by_lua '
+            local s = ""
+
+            local function fail(...)
+                ngx.log(ngx.ERR, ...)
+            end
+
+            local function g()
+                s = s .. "[g]"
+                print("trace: ", s)
+            end
+
+            local function f()
+                local ok, err = ngx.timer.at(0.01, g)
+                if not ok then
+                    fail("failed to set timer: ", err)
+                    return
+                end
+                s = s .. "[f]"
+            end
+            local ok, err = ngx.timer.at(0.01, f)
+            if not ok then
+                ngx.say("failed to set timer: ", err)
+                return
+            end
+            ngx.say("registered timer")
+            s = "[m]"
+        ';
+    }
+--- request
+GET /t
+
+--- stap2 eval: $::StapScript
+--- stap eval: $::GCScript
+--- stap_out
+create 2 in 1
+terminate 1: ok
+delete thread 1
+create 3 in 2
+terminate 2: ok
+delete thread 2
+terminate 3: ok
+delete thread 3
+
+--- response_body
+registered timer
+
+--- wait: 0.1
+--- no_error_log
+[error]
+--- error_log eval
+[
+"lua ngx.timer expired",
+"http lua close fake http connection",
+"trace: [m][f][g]",
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
+"lua close the global Lua VM",
+]
+
+
+
+=== TEST 25: lua variable sharing via upvalue
+--- http_config
+    lua_code_cache off;
+--- config
+    location /t {
+        content_by_lua '
+            local begin = ngx.now()
+            local foo
+            local function f()
+                foo = 3
+                print("elapsed: ", ngx.now() - begin)
+            end
+            local ok, err = ngx.timer.at(0.05, f)
+            if not ok then
+                ngx.say("failed to set timer: ", err)
+                return
+            end
+            ngx.say("registered timer")
+            ngx.sleep(0.06)
+            ngx.say("foo = ", foo)
+        ';
+    }
+--- request
+GET /t
+--- stap2
+F(ngx_http_lua_timer_handler) {
+    println("lua timer handler")
+}
+
+--- response_body
+registered timer
+foo = 3
+
+--- wait: 0.1
+--- no_error_log
+[error]
+
+--- error_log eval
+[
+qr/\[lua\] \[string "content_by_lua"\]:\d+: elapsed: 0\.0(?:4[4-9]|5[0-6])/,
+"lua ngx.timer expired",
+"http lua close fake http connection",
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
+"lua close the global Lua VM",
+]
+
+
+
+=== TEST 26: lua_max_running_timers (just not enough)
+--- http_config
+    lua_max_running_timers 1;
+--- config
+    lua_code_cache off;
+    location /t {
+        content_by_lua '
+            local s = ""
+
+            local function fail(...)
+                ngx.log(ngx.ERR, ...)
+            end
+
+            local f, g
+
+            g = function ()
+                ngx.sleep(0.01)
+            end
+
+            f = function ()
+                ngx.sleep(0.01)
+            end
+            local ok, err = ngx.timer.at(0, f)
+            if not ok then
+                ngx.say("failed to set timer f: ", err)
+                return
+            end
+            local ok, err = ngx.timer.at(0, g)
+            if not ok then
+                ngx.say("failed to set timer g: ", err)
+                return
+            end
+            ngx.say("registered timer")
+            s = "[m]"
+        ';
+    }
+--- request
+GET /t
+
+--- stap2 eval: $::StapScript
+--- stap eval: $::GCScript
+--- stap_out
+create 2 in 1
+create 3 in 1
+terminate 1: ok
+delete thread 1
+terminate 2: ok
+delete thread 2
+
+--- response_body
+registered timer
+
+--- wait: 0.1
+--- no_error_log
+[crit]
+[error]
+
+--- error_log eval
+[
+"1 lua_max_running_timers are not enough",
+"lua ngx.timer expired",
+"http lua close fake http connection",
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
+]
+
+
+
+=== TEST 27: GC issue with the on_abort thread object
+--- config
+    lua_code_cache off;
+    location = /t {
+        lua_check_client_abort on;
+        content_by_lua '
+            ngx.on_abort(function () end)
+            collectgarbage()
+            ngx.sleep(1)
+        ';
+    }
+--- request
+    GET /t
+--- abort
+--- timeout: 0.2
+--- wait: 1
+--- ignore_response
+--- no_error_log
+[error]
+--- error_log eval
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/
 

src/ngx_http_lua_accessby.c
@@ -155,8 +155,7 @@ ngx_http_lua_access_handler_inline(ngx_http_request_t *r)
     rc = ngx_http_lua_cache_loadbuffer(L, llcf->access_src.value.data,
                                        llcf->access_src.value.len,
                                        llcf->access_src_key,
-                                       "access_by_lua",
-                                       llcf->enable_code_cache ? 1 : 0);
+                                       "access_by_lua");
 
     if (rc != NGX_OK) {
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
@@ -192,9 +191,7 @@ ngx_http_lua_access_handler_file(ngx_http_request_t *r)
     L = ngx_http_lua_get_main_lua_state(r);
 
     /*  load Lua script file (w/ cache)        sp = 1 */
-    rc = ngx_http_lua_cache_loadfile(L, script_path, llcf->access_src_key,
-                                     llcf->enable_code_cache ? 1 : 0);
-
+    rc = ngx_http_lua_cache_loadfile(L, script_path, llcf->access_src_key);
     if (rc != NGX_OK) {
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
     }

src/ngx_http_lua_bodyfilterby.c
@@ -165,9 +165,7 @@ ngx_http_lua_body_filter_inline(ngx_http_request_t *r, ngx_chain_t *in)
     rc = ngx_http_lua_cache_loadbuffer(L, llcf->body_filter_src.value.data,
                                        llcf->body_filter_src.value.len,
                                        llcf->body_filter_src_key,
-                                       "body_filter_by_lua",
-                                       llcf->enable_code_cache ? 1 : 0);
-
+                                       "body_filter_by_lua");
     if (rc != NGX_OK) {
         return NGX_ERROR;
     }
@@ -213,9 +211,7 @@ ngx_http_lua_body_filter_file(ngx_http_request_t *r, ngx_chain_t *in)
 
     /*  load Lua script file (w/ cache)        sp = 1 */
     rc = ngx_http_lua_cache_loadfile(L, script_path,
-                                     llcf->body_filter_src_key,
-                                     llcf->enable_code_cache ? 1 : 0);
-
+                                     llcf->body_filter_src_key);
     if (rc != NGX_OK) {
         return NGX_ERROR;
     }

src/ngx_http_lua_cache.c
@@ -19,9 +19,6 @@
 #include "ngx_http_lua_util.h"
 
 
-static void ngx_http_lua_clear_package_loaded(lua_State *L);
-
-
 /**
  * Find code chunk associated with the given key in code cache,
  * and push it to the top of Lua stack if found.
@@ -121,7 +118,7 @@ ngx_http_lua_cache_store_code(lua_State *L, const char *key)
 
 ngx_int_t
 ngx_http_lua_cache_loadbuffer(lua_State *L, const u_char *src, size_t src_len,
-    const u_char *cache_key, const char *name, unsigned enabled)
+    const u_char *cache_key, const char *name)
 {
     int          rc, n;
     const char  *err = NULL;
@@ -129,10 +126,6 @@ ngx_http_lua_cache_loadbuffer(lua_State *L, const u_char *src, size_t src_len,
     n = lua_gettop(L);
 
     dd("XXX cache key: [%s]", cache_key);
-
-    if (!enabled) {
-        ngx_http_lua_clear_package_loaded(L);
-    }
 
     if (ngx_http_lua_cache_load_code(L, (char *) cache_key) == NGX_OK) {
         /*  code chunk loaded from cache, sp++ */
@@ -185,45 +178,40 @@ error:
 
 ngx_int_t
 ngx_http_lua_cache_loadfile(lua_State *L, const u_char *script,
-    const u_char *cache_key, unsigned enabled)
+    const u_char *cache_key)
 {
-    int              rc;
+    int              rc, n;
     u_char          *p;
     u_char           buf[NGX_HTTP_LUA_FILE_KEY_LEN + 1];
-    int              n;
     const char      *err = NULL;
 
     n = lua_gettop(L);
 
     /*  calculate digest of script file path */
-    dd("code cache enabled: %d", (int) enabled);
+    if (cache_key == NULL) {
+        dd("CACHE file key not pre-calculated...calculating");
+        p = ngx_copy(buf, NGX_HTTP_LUA_FILE_TAG, NGX_HTTP_LUA_FILE_TAG_LEN);
 
-    if (enabled) {
-        if (cache_key == NULL) {
-            dd("CACHE file key not pre-calculated...calculating");
-            p = ngx_copy(buf, NGX_HTTP_LUA_FILE_TAG, NGX_HTTP_LUA_FILE_TAG_LEN);
+        p = ngx_http_lua_digest_hex(p, script, ngx_strlen(script));
 
-            p = ngx_http_lua_digest_hex(p, script, ngx_strlen(script));
+        *p = '\0';
+        cache_key = buf;
 
-            *p = '\0';
-            cache_key = buf;
-
-        } else {
-            dd("CACHE file key already pre-calculated");
-        }
-
-        dd("XXX cache key for file: [%s]", cache_key);
-
-        if (ngx_http_lua_cache_load_code(L, (char *) cache_key) == NGX_OK) {
-            /*  code chunk loaded from cache, sp++ */
-            dd("Code cache hit! cache key='%s', stack top=%d, file path='%s'",
-               cache_key, lua_gettop(L), script);
-            return NGX_OK;
-        }
-
-        dd("Code cache missed! cache key='%s', stack top=%d, file path='%s'",
-           cache_key, lua_gettop(L), script);
+    } else {
+        dd("CACHE file key already pre-calculated");
     }
+
+    dd("XXX cache key for file: [%s]", cache_key);
+
+    if (ngx_http_lua_cache_load_code(L, (char *) cache_key) == NGX_OK) {
+        /*  code chunk loaded from cache, sp++ */
+        dd("Code cache hit! cache key='%s', stack top=%d, file path='%s'",
+           cache_key, lua_gettop(L), script);
+        return NGX_OK;
+    }
+
+    dd("Code cache missed! cache key='%s', stack top=%d, file path='%s'",
+       cache_key, lua_gettop(L), script);
 
     /*  load closure factory of script file to the top of lua stack, sp++ */
     rc = ngx_http_lua_clfactory_loadfile(L, (char *) script);
@@ -245,25 +233,13 @@ ngx_http_lua_cache_loadfile(lua_State *L, const u_char *script,
         goto error;
     }
 
-    if (enabled) {
-        /*  store closure factory and gen new closure at the top of lua stack
-         *  to code cache */
-        rc = ngx_http_lua_cache_store_code(L, (char *) cache_key);
+    /*  store closure factory and gen new closure at the top of lua stack
+     *  to code cache */
+    rc = ngx_http_lua_cache_store_code(L, (char *) cache_key);
 
-        if (rc != NGX_OK) {
-            err = "fail to generate new closure from the closure factory";
-            goto error;
-        }
-
-    } else {
-        /*  call closure factory to generate new closure */
-        rc = lua_pcall(L, 0, 1, 0);
-        if (rc != 0) {
-            err = "failed to call closure factory";
-            goto error;
-        }
-
-        ngx_http_lua_clear_package_loaded(L);
+    if (rc != NGX_OK) {
+        err = "fail to generate new closure from the closure factory";
+        goto error;
     }
 
     return NGX_OK;
@@ -274,145 +250,6 @@ error:
 
     lua_settop(L, n);
     return NGX_ERROR;
-}
-
-
-static void
-ngx_http_lua_clear_package_loaded(lua_State *L)
-{
-    size_t       len;
-    u_char      *p;
-
-    dd("clear out package.loaded.* on the Lua land");
-    lua_getglobal(L, "package"); /* package */
-    lua_getfield(L, -1, "loaded"); /* package loaded */
-    lua_pushnil(L); /* package loaded nil */
-
-    while (lua_next(L, -2)) { /* package loaded key value */
-        lua_pop(L, 1);  /* package loaded key */
-
-        p = (u_char *) lua_tolstring(L, -1, &len);
-
-#if 1
-        /* XXX work-around the "stack overflow" issue of LuaRocks
-         * while unloading and reloading Lua modules */
-        if (len >= sizeof("luarocks") - 1
-            && ngx_strncmp(p, "luarocks", sizeof("luarocks") - 1) == 0)
-        {
-            goto done;
-        }
-#endif
-
-        switch (len) {
-        case 2:
-            if (p[0] == 'o' && p[1] == 's') {
-                goto done;
-            }
-
-            if (p[0] == 'i' && p[1] == 'o') {
-                goto done;
-            }
-
-#if 0
-            if (ngx_strncmp(p, "_G", sizeof("_G") - 1) == 0) {
-                goto done;
-            }
-#endif
-
-            break;
-
-        case 3:
-            if (ngx_strncmp(p, "bit", sizeof("bit") - 1) == 0) {
-                goto done;
-            }
-
-            if (ngx_strncmp(p, "jit", sizeof("jit") - 1) == 0) {
-                goto done;
-            }
-
-            if (ngx_strncmp(p, "ngx", sizeof("ngx") - 1) == 0) {
-                goto done;
-            }
-
-            if (ngx_strncmp(p, "ndk", sizeof("ndk") - 1) == 0) {
-                goto done;
-            }
-
-            break;
-
-        case 4:
-            if (ngx_strncmp(p, "math", sizeof("math") - 1) == 0) {
-                goto done;
-            }
-
-            break;
-
-        case 5:
-            if (ngx_strncmp(p, "table", sizeof("table") - 1) == 0) {
-                goto done;
-            }
-
-            if (ngx_strncmp(p, "debug", sizeof("table") - 1) == 0) {
-                goto done;
-            }
-
-            break;
-
-        case 6:
-            if (ngx_strncmp(p, "string", sizeof("string") - 1) == 0) {
-                goto done;
-            }
-
-            break;
-
-        case 7:
-            if (ngx_strncmp(p, "package", sizeof("package") - 1) == 0) {
-                goto done;
-            }
-
-            if (ngx_strncmp(p, "jit.opt", sizeof("jit.opt") - 1) == 0) {
-                goto done;
-            }
-
-            break;
-
-       case 8:
-            if (ngx_strncmp(p, "jit.util", sizeof("jit.util") - 1) == 0) {
-                goto done;
-            }
-
-            break;
-
-       case 9:
-            if (ngx_strncmp(p, "coroutine", sizeof("coroutine") - 1) == 0) {
-                goto done;
-            }
-
-            break;
-
-        default:
-            break;
-        }
-
-        dd("clearing package %s", p);
-
-        lua_pushvalue(L, -1);  /* package loaded key key */
-        lua_pushnil(L); /* package loaded key key nil */
-        lua_settable(L, -4);  /* package loaded key */
-done:
-        continue;
-    }
-
-    /* package loaded */
-    lua_pop(L, 2);
-
-    /* force a full GC cycle */
-    lua_gc(L, LUA_GCCOLLECT, 0);
-
-#if 0
-    lua_newtable(L);
-    lua_setglobal(L, "_G");
-#endif
 }
 
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */

commit f61d7ba10df2e83a25eba779bd8dbd6af35a2cff

bugfix: turning off lua_code_cache and using cosocket connection pools might lead to memory issues (like segfaults). this regression had appeared in commit 5fc80b5b0. thanks Zheng Ping for the report.

src/ngx_http_lua_socket_tcp.c
@@ -3552,6 +3552,7 @@ static int ngx_http_lua_socket_tcp_setkeepalive(lua_State *L)
 
         spool->conf = lmcf;
         spool->active_connections = 0;
+        spool->lua_vm = ngx_http_lua_get_main_lua_state(r);
 
         ngx_queue_init(&spool->cache);
         ngx_queue_init(&spool->free);
@@ -3855,7 +3856,7 @@ ngx_http_lua_socket_free_pool(ngx_log_t *log, ngx_http_lua_socket_pool_t *spool)
                    "lua tcp socket keepalive: free connection pool for \"%s\"",
                    spool->key);
 
-    L = spool->conf->lua;
+    L = spool->lua_vm;
 
     lua_pushlightuserdata(L, &ngx_http_lua_socket_pool_key);
     lua_rawget(L, LUA_REGISTRYINDEX);

src/ngx_http_lua_socket_tcp.h
@@ -35,6 +35,7 @@ typedef void (*ngx_http_lua_socket_tcp_upstream_handler_pt)(
 
 
 typedef struct {
+    lua_State                         *lua_vm;
     ngx_http_lua_main_conf_t          *conf;
 
     /* active connections == out-of-pool reused connections

src/ngx_http_lua_util.c
@@ -120,6 +120,7 @@ static ngx_int_t
     ngx_http_lua_ctx_t *ctx);
 static lua_State * ngx_http_lua_new_state(ngx_cycle_t *cycle,
     ngx_http_lua_main_conf_t *lmcf, ngx_log_t *log);
+static void ngx_http_lua_cleanup_conn_pools(lua_State *L);
 
 
 #ifndef LUA_PATH_SEP
@@ -3697,12 +3698,48 @@ ngx_http_lua_cleanup_vm(void *data)
 
         if (--cln_data->count == 0) {
             L = cln_data->state;
+            ngx_http_lua_cleanup_conn_pools(L);
             ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
                            "lua close the global Lua VM %p", L);
             lua_close(L);
             ngx_free(cln_data);
         }
     }
+}
+
+
+static void
+ngx_http_lua_cleanup_conn_pools(lua_State *L)
+{
+    ngx_queue_t                         *q;
+    ngx_connection_t                    *c;
+    ngx_http_lua_socket_pool_t          *spool;
+    ngx_http_lua_socket_pool_item_t     *item;
+
+    lua_pushlightuserdata(L, &ngx_http_lua_socket_pool_key);
+    lua_rawget(L, LUA_REGISTRYINDEX); /* table */
+
+    lua_pushnil(L);  /* first key */
+    while (lua_next(L, -2) != 0) {
+        /* tb key val */
+        spool = lua_touserdata(L, -1);
+
+        if (!ngx_queue_empty(&spool->cache)) {
+            q = ngx_queue_head(&spool->cache);
+            item = ngx_queue_data(q, ngx_http_lua_socket_pool_item_t, queue);
+            c = item->connection;
+            ngx_close_connection(c);
+            ngx_queue_remove(q);
+
+            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
+                           "lua tcp socket keepalive: free connection pool "
+                           "for \"%s\"", spool->key);
+        }
+
+        lua_pop(L, 1);
+    }
+
+    lua_pop(L, 1);
 }
 
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */

t/025-codecache.t
@@ -5,13 +5,15 @@ use t::TestNginxLua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * 130;
+plan tests => repeat_each() * 144;
 
 #$ENV{LUA_PATH} = $ENV{HOME} . '/work/JSON4Lua-0.9.30/json/?.lua';
 
 no_long_string();
 
 our $HtmlDir = html_dir;
+
+$ENV{TEST_NGINX_MEMCACHED_PORT} ||= 11211;
 
 check_accum_error_log();
 run_tests();
@@ -1041,5 +1043,141 @@ decrementing the reference count for Lua VM: 4
 "decrementing the reference count for Lua VM: 1",
 qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
 "lua close the global Lua VM",
+]
+
+
+
+=== TEST 29: cosocket connection pool timeout (after Lua VM destroys)
+--- http_config eval
+    "lua_package_path '$::HtmlDir/?.lua;./?.lua';"
+--- config
+    lua_code_cache off;
+    location = /t {
+        set $port $TEST_NGINX_MEMCACHED_PORT;
+        content_by_lua '
+            local test = require "test"
+            local port = ngx.var.port
+            test.go(port)
+        ';
+    }
+--- user_files
+>>> test.lua
+module("test", package.seeall)
+
+function go(port)
+    local sock = ngx.socket.tcp()
+    local ok, err = sock:connect("127.0.0.1", port)
+    if not ok then
+        ngx.say("failed to connect: ", err)
+        return
+    end
+
+    ngx.say("connected: ", ok, ", reused: ", sock:getreusedtimes())
+
+    local req = "flush_all\r\n"
+
+    local bytes, err = sock:send(req)
+    if not bytes then
+        ngx.say("failed to send request: ", err)
+        return
+    end
+    ngx.say("request sent: ", bytes)
+
+    local line, err, part = sock:receive()
+    if line then
+        ngx.say("received: ", line)
+
+    else
+        ngx.say("failed to receive a line: ", err, " [", part, "]")
+    end
+
+    local ok, err = sock:setkeepalive(1)
+    if not ok then
+        ngx.say("failed to set reusable: ", err)
+    end
+end
+--- request
+GET /t
+--- response_body
+connected: 1, reused: 0
+request sent: 11
+received: OK
+--- no_error_log
+[error]
+lua tcp socket keepalive max idle timeout
+
+--- error_log eval
+[
+qq{lua tcp socket keepalive create connection pool for key "127.0.0.1:$ENV{TEST_NGINX_MEMCACHED_PORT}"},
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
+"lua tcp socket keepalive: free connection pool for ",
+]
+
+
+
+=== TEST 30: cosocket connection pool timeout (before Lua VM destroys)
+--- http_config eval
+    "lua_package_path '$::HtmlDir/?.lua;./?.lua';"
+--- config
+    lua_code_cache off;
+    location = /t {
+        set $port $TEST_NGINX_MEMCACHED_PORT;
+        content_by_lua '
+            local test = require "test"
+            local port = ngx.var.port
+            test.go(port)
+        ';
+    }
+--- user_files
+>>> test.lua
+module("test", package.seeall)
+
+function go(port)
+    local sock = ngx.socket.tcp()
+    local ok, err = sock:connect("127.0.0.1", port)
+    if not ok then
+        ngx.say("failed to connect: ", err)
+        return
+    end
+
+    ngx.say("connected: ", ok, ", reused: ", sock:getreusedtimes())
+
+    local req = "flush_all\r\n"
+
+    local bytes, err = sock:send(req)
+    if not bytes then
+        ngx.say("failed to send request: ", err)
+        return
+    end
+    ngx.say("request sent: ", bytes)
+
+    local line, err, part = sock:receive()
+    if line then
+        ngx.say("received: ", line)
+
+    else
+        ngx.say("failed to receive a line: ", err, " [", part, "]")
+    end
+
+    local ok, err = sock:setkeepalive(1)
+    if not ok then
+        ngx.say("failed to set reusable: ", err)
+    end
+    ngx.sleep(0.01)
+end
+--- request
+GET /t
+--- response_body
+connected: 1, reused: 0
+request sent: 11
+received: OK
+--- no_error_log
+[error]
+--- error_log eval
+[
+qq{lua tcp socket keepalive create connection pool for key "127.0.0.1:$ENV{TEST_NGINX_MEMCACHED_PORT}"},
+qr/\[alert\] \S+ lua_code_cache is off; this will hurt performance/,
+"lua tcp socket keepalive: free connection pool for ",
+"lua tcp socket keepalive max idle timeout",
 ]
 