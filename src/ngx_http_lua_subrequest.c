7aa50bfc997510eeb440d464f67b204db248c091
aviramc <aviram@adallom.com>
2013-12-18 19:01:35 +0200

    Now when creating a subrequest, we now perform a deep copy of the headers of the main request. This means that all headers are actually copied, and not their referernces.
    The call to ngx_http_lua_set_input_header() will also make built-in headers to be set
    correctly within the context of the subrequest.

--- src/ngx_http_lua_subrequest.c
+++ src/ngx_http_lua_subrequest.c
@@ -15,6 +15,7 @@
 #include "ngx_http_lua_util.h"
 #include "ngx_http_lua_ctx.h"
 #include "ngx_http_lua_contentby.h"
+#include "ngx_http_lua_headers_in.h"
 #if defined(NGX_DTRACE) && NGX_DTRACE
 #include "ngx_http_probe.h"
 #endif
@@ -626,7 +627,8 @@ ngx_http_lua_adjust_subrequest(ngx_http_request_t *sr, ngx_uint_t method,
 
     r = sr->parent;
 
-    sr->header_in = r->header_in;
+    sr->headers_in.content_length_n = -1;
+    sr->headers_in.keep_alive_n = -1;
 
     if (body) {
         sr->request_body = body;
@@ -1203,12 +1205,7 @@ ngx_http_lua_set_content_length_header(ngx_http_request_t *r, off_t len)
             continue;
         }
 
-        h = ngx_list_push(&r->headers_in.headers);
-        if (h == NULL) {
-            return NGX_ERROR;
-        }
-
-        *h = header[i];
+        ngx_http_lua_set_input_header(r, header[i].key, header[i].value, 1);
     }
 
     /* XXX maybe we should set those built-in header slot in
@@ -1491,8 +1488,6 @@ ngx_http_lua_subrequest(ngx_http_request_t *r,
 
     sr->pool = r->pool;
 
-    sr->headers_in = r->headers_in;
-
     ngx_http_clear_content_length(sr);
     ngx_http_clear_accept_ranges(sr);
     ngx_http_clear_last_modified(sr);
@@ -1703,7 +1698,7 @@ ngx_http_lua_copy_in_file_request_body(ngx_http_request_t *r)
 static ngx_int_t
 ngx_http_lua_copy_request_headers(ngx_http_request_t *sr, ngx_http_request_t *r)
 {
-    ngx_table_elt_t                 *h, *header;
+    ngx_table_elt_t                 *header;
     ngx_list_part_t                 *part;
     ngx_uint_t                       i;
 
@@ -1728,12 +1723,7 @@ ngx_http_lua_copy_request_headers(ngx_http_request_t *sr, ngx_http_request_t *r)
             i = 0;
         }
 
-        h = ngx_list_push(&sr->headers_in.headers);
-        if (h == NULL) {
-            return NGX_ERROR;
-        }
-
-        *h = header[i];
+        ngx_http_lua_set_input_header(sr, header[i].key, header[i].value, 1);
     }
 
     /* XXX we should set those built-in header slot in
