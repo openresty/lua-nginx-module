openresty/lua-nginx-module

commit e768e151ab33e4b5b32fb0972b2d52bafb6d0966

bugfix: segfault might happen when aborting a "light thread" pending on downstream cosocket writes. thanks Aviram Cohen for the report.

src/ngx_http_lua_socket_tcp.c
@@ -40,7 +40,6 @@ static void ngx_http_lua_socket_send_handler(ngx_http_request_t *r,
 static void ngx_http_lua_socket_connected_handler(ngx_http_request_t *r,
     ngx_http_lua_socket_tcp_upstream_t *u);
 static void ngx_http_lua_socket_tcp_cleanup(void *data);
-static void ngx_http_lua_req_socket_cleanup(void *data);
 static void ngx_http_lua_socket_tcp_finalize(ngx_http_request_t *r,
     ngx_http_lua_socket_tcp_upstream_t *u);
 static ngx_int_t ngx_http_lua_socket_send(ngx_http_request_t *r,
@@ -2471,6 +2470,15 @@ ngx_http_lua_socket_tcp_finalize(ngx_http_request_t *r,
     }
 
     if (u->raw_downstream || u->body_downstream) {
+        if (ctx && ctx->writing_raw_req_socket) {
+            ctx->writing_raw_req_socket = 0;
+            if (r->connection->write->timer_set) {
+                ngx_del_timer(r->connection->write);
+            }
+
+            r->connection->write->error = 1;
+        }
+
         if (r->connection->read->timer_set) {
             ngx_del_timer(r->connection->read);
         }
@@ -3384,7 +3392,7 @@ ngx_http_lua_req_socket(lua_State *L)
         return 2;
     }
 
-    cln->handler = ngx_http_lua_req_socket_cleanup;
+    cln->handler = ngx_http_lua_socket_tcp_cleanup;
     cln->data = u;
     u->cleanup = &cln->handler;
 
@@ -3957,36 +3965,10 @@ ngx_http_lua_socket_downstream_destroy(lua_State *L)
     }
 
     if (u->cleanup) {
-        ngx_http_lua_req_socket_cleanup(u); /* it will clear u->cleanup */
+        ngx_http_lua_tcp_socket_cleanup(u); /* it will clear u->cleanup */
     }
 
     return 0;
-}
-
-
-static void
-ngx_http_lua_req_socket_cleanup(void *data)
-{
-    ngx_http_lua_socket_tcp_upstream_t  *u = data;
-
-#if (NGX_DEBUG)
-    ngx_http_request_t  *r;
-
-    r = u->request;
-
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "cleanup lua tcp socket downstream request: \"%V\"",
-                   &r->uri);
-#endif
-
-    if (u->cleanup) {
-        *u->cleanup = NULL;
-        u->cleanup = NULL;
-    }
-
-    if (u->peer.connection) {
-        u->peer.connection = NULL;
-    }
 }
 
 

t/065-tcp-socket-timeout.t
@@ -29,7 +29,7 @@ our $StapScript = $t::StapThread::StapScript;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 4 + 11);
+plan tests => repeat_each() * (blocks() * 4 + 12);
 
 our $HtmlDir = html_dir;
 
@@ -770,4 +770,112 @@ failed to send again: closed
 lua tcp socket send timeout: 100
 lua tcp socket connect timeout: 60000
 lua tcp socket write timed out
+
+
+
+=== TEST 19: abort when upstream sockets pending on writes
+--- config
+    server_tokens off;
+    resolver $TEST_NGINX_RESOLVER;
+    location /t {
+        content_by_lua '
+            local sock = ngx.socket.tcp()
+            local ok, err = sock:connect("127.0.0.1", $TEST_NGINX_MEMCACHED_PORT)
+            if not ok then
+                ngx.say("failed to connect: ", err)
+                return
+            end
+
+            ngx.say("connected: ", ok)
+
+            sock:settimeout(100)
+            ngx.thread.spawn(function () ngx.sleep(0.001) ngx.say("done") ngx.exit(200) end)
+            local bytes
+            bytes, err = sock:send("get helloworld!")
+            if bytes then
+                ngx.say("sent: ", bytes)
+            else
+                ngx.say("failed to send: ", err)
+            end
+        ';
+    }
+--- request
+GET /t
+--- stap2
+global active = 0
+F(ngx_http_lua_socket_send) {
+    active = 1
+    println(probefunc())
+}
+probe syscall.send,
+    syscall.sendto,
+    syscall.writev
+{
+    if (active && pid() == target()) {
+        println(probefunc())
+    }
+}
+--- response_body
+connected: 1
+done
+--- error_log
+lua tcp socket send timeout: 100
+lua tcp socket connect timeout: 60000
+--- no_error_log
+lua tcp socket write timed out
+
+
+
+=== TEST 20: abort when downstream socket pending on writes
+--- config
+    server_tokens off;
+    resolver $TEST_NGINX_RESOLVER;
+    location /t {
+        content_by_lua '
+            ngx.send_headers()
+            ngx.flush(true)
+            local sock, err = ngx.req.socket(true)
+            if not sock then
+                ngx.say("failed to acquire the req socket: ", err)
+                return
+            end
+
+            sock:settimeout(100)
+            ngx.thread.spawn(function ()
+                ngx.sleep(0.001)
+                ngx.log(ngx.WARN, "quitting request now")
+                ngx.exit(200)
+            end)
+            local bytes
+            bytes, err = sock:send("e\\r\\nget helloworld!")
+            if bytes then
+                ngx.say("sent: ", bytes)
+            else
+                ngx.say("failed to send: ", err)
+            end
+        ';
+    }
+--- request
+GET /t
+--- stap2
+global active = 0
+F(ngx_http_lua_socket_send) {
+    active = 1
+    println(probefunc())
+}
+probe syscall.send,
+    syscall.sendto,
+    syscall.writev
+{
+    if (active && pid() == target()) {
+        println(probefunc())
+    }
+}
+--- ignore_response
+--- error_log
+lua tcp socket send timeout: 100
+quitting request now
+--- no_error_log
+lua tcp socket write timed out
+[alert]
 

commit 60999efe35d86282320732e461656e513edb2b16

bugfix: segmentation fault might happen when the downstream cosocket objects were GC'd. this regression had appeared in commit e768e151ab3.

src/ngx_http_lua_socket_tcp.c
@@ -102,7 +102,7 @@ static ngx_int_t ngx_http_lua_socket_insert_buffer(ngx_http_request_t *r,
     ngx_http_lua_socket_tcp_upstream_t *u, u_char *pat, size_t prefix);
 static ngx_int_t ngx_http_lua_socket_tcp_resume(ngx_http_request_t *r);
 static void ngx_http_lua_tcp_resolve_cleanup(void *data);
-static void ngx_http_lua_tcp_socket_cleanup(void *data);
+static void ngx_http_lua_coctx_cleanup(void *data);
 
 
 enum {
@@ -971,7 +971,7 @@ ngx_http_lua_socket_resolve_retval_handler(ngx_http_request_t *r,
 
     /* rc == NGX_AGAIN */
 
-    coctx->cleanup = ngx_http_lua_tcp_socket_cleanup;
+    coctx->cleanup = ngx_http_lua_coctx_cleanup;
 
     ngx_add_timer(c->write, u->connect_timeout);
 
@@ -1256,7 +1256,7 @@ ngx_http_lua_socket_tcp_receive(lua_State *L)
     u->read_event_handler = ngx_http_lua_socket_read_handler;
     u->write_event_handler = ngx_http_lua_socket_dummy_handler;
 
-    ctx->cur_co_ctx->cleanup = ngx_http_lua_tcp_socket_cleanup;
+    ctx->cur_co_ctx->cleanup = ngx_http_lua_coctx_cleanup;
 
     if (ctx->entered_content_phase) {
         r->write_event_handler = ngx_http_lua_content_wev_handler;
@@ -1854,7 +1854,7 @@ ngx_http_lua_socket_tcp_send(lua_State *L)
 
     /* rc == NGX_AGAIN */
 
-    ctx->cur_co_ctx->cleanup = ngx_http_lua_tcp_socket_cleanup;
+    ctx->cur_co_ctx->cleanup = ngx_http_lua_coctx_cleanup;
     if (u->raw_downstream) {
         ctx->writing_raw_req_socket = 1;
     }
@@ -2426,7 +2426,7 @@ ngx_http_lua_socket_tcp_cleanup(void *data)
     r = u->request;
 
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "cleanup lua tcp socket upstream request: \"%V\"", &r->uri);
+                   "cleanup lua tcp socket request: \"%V\"", &r->uri);
 
     ngx_http_lua_socket_tcp_finalize(r, u);
 }
@@ -2441,8 +2441,15 @@ ngx_http_lua_socket_tcp_finalize(ngx_http_request_t *r,
     ngx_chain_t                        **ll;
     ngx_http_lua_ctx_t                  *ctx;
 
+    dd("request: %p, u: %p, u->cleanup: %p", r, u, u->cleanup);
+
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
                    "lua finalize socket");
+
+    if (u->cleanup) {
+        *u->cleanup = NULL;
+        u->cleanup = NULL;
+    }
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_lua_module);
 
@@ -2462,11 +2469,6 @@ ngx_http_lua_socket_tcp_finalize(ngx_http_request_t *r,
         u->bufs_in = NULL;
         u->buf_in = NULL;
         ngx_memzero(&u->buffer, sizeof(ngx_buf_t));
-    }
-
-    if (u->cleanup) {
-        *u->cleanup = NULL;
-        u->cleanup = NULL;
     }
 
     if (u->raw_downstream || u->body_downstream) {
@@ -2813,7 +2815,7 @@ ngx_http_lua_socket_receiveuntil_iterator(lua_State *L)
     u->read_event_handler = ngx_http_lua_socket_read_handler;
     u->write_event_handler = ngx_http_lua_socket_dummy_handler;
 
-    ctx->cur_co_ctx->cleanup = ngx_http_lua_tcp_socket_cleanup;
+    ctx->cur_co_ctx->cleanup = ngx_http_lua_coctx_cleanup;
 
     if (ctx->entered_content_phase) {
         r->write_event_handler = ngx_http_lua_content_wev_handler;
@@ -3957,15 +3959,16 @@ ngx_http_lua_socket_downstream_destroy(lua_State *L)
 {
     ngx_http_lua_socket_tcp_upstream_t     *u;
 
-    dd("upstream destroy triggered by Lua GC");
+    dd("downstream destory");
 
     u = lua_touserdata(L, 1);
     if (u == NULL) {
+        dd("u is NULL");
         return 0;
     }
 
     if (u->cleanup) {
-        ngx_http_lua_tcp_socket_cleanup(u); /* it will clear u->cleanup */
+        ngx_http_lua_socket_tcp_cleanup(u); /* it will clear u->cleanup */
     }
 
     return 0;
@@ -4287,10 +4290,12 @@ ngx_http_lua_tcp_resolve_cleanup(void *data)
 
 
 static void
-ngx_http_lua_tcp_socket_cleanup(void *data)
+ngx_http_lua_coctx_cleanup(void *data)
 {
     ngx_http_lua_socket_tcp_upstream_t      *u;
     ngx_http_lua_co_ctx_t                   *coctx = data;
+
+    dd("running coctx cleanup");
 
     u = coctx->data;
     if (u == NULL) {

t/067-req-socket.t
@@ -5,7 +5,7 @@ use Test::Nginx::Socket::Lua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 3 + 8);
+plan tests => repeat_each() * (blocks() * 3 + 9);
 
 our $HtmlDir = html_dir;
 
@@ -1064,4 +1064,37 @@ received: received: hello world
 
 --- error_log
 lua tcp socket read timed out
+
+
+
+=== TEST 17: req socket GC'd
+--- config
+    location /t {
+        content_by_lua '
+            do
+                local sock, err = ngx.req.socket()
+                if sock then
+                    ngx.say("got the request socket")
+                else
+                    ngx.say("failed to get the request socket: ", err)
+                end
+            end
+            collectgarbage()
+            ngx.log(ngx.WARN, "GC cycle done")
+
+            ngx.say("done")
+        ';
+    }
+--- request
+POST /t
+hello world
+--- response_body
+got the request socket
+done
+--- no_error_log
+[error]
+--- grep_error_log eval: qr/lua finalize socket|GC cycle done/
+--- grep_error_log_out
+lua finalize socket
+GC cycle done
 