openresty/lua-nginx-module

commit a6a0ed52007125fd7bb25be3094d4bd2a1eccacc

bugfix: memory leak cound happen when both cosocket connection pools and "lua_code_cache off" were used. this regression had appeared in the ssl cosocket patch, i.e., commit 5aed1966.

src/ngx_http_lua_socket_tcp.c
@@ -3269,6 +3269,7 @@ ngx_http_lua_socket_tcp_close_connection(ngx_connection_t *c)
 
     if (c->pool) {
         ngx_destroy_pool(c->pool);
+        c->pool = NULL;
     }
 
     ngx_close_connection(c);
@@ -5156,6 +5157,43 @@ ngx_http_lua_ssl_free_session(lua_State *L)
     }
 
     return 0;
+}
+
+
+void
+ngx_http_lua_cleanup_conn_pools(lua_State *L)
+{
+    ngx_queue_t                         *q;
+    ngx_connection_t                    *c;
+    ngx_http_lua_socket_pool_t          *spool;
+    ngx_http_lua_socket_pool_item_t     *item;
+
+    lua_pushlightuserdata(L, &ngx_http_lua_socket_pool_key);
+    lua_rawget(L, LUA_REGISTRYINDEX); /* table */
+
+    lua_pushnil(L);  /* first key */
+    while (lua_next(L, -2) != 0) {
+        /* tb key val */
+        spool = lua_touserdata(L, -1);
+
+        if (!ngx_queue_empty(&spool->cache)) {
+            q = ngx_queue_head(&spool->cache);
+            item = ngx_queue_data(q, ngx_http_lua_socket_pool_item_t, queue);
+            c = item->connection;
+
+            ngx_http_lua_socket_tcp_close_connection(c);
+
+            ngx_queue_remove(q);
+
+            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
+                           "lua tcp socket keepalive: free connection pool "
+                           "for \"%s\"", spool->key);
+        }
+
+        lua_pop(L, 1);
+    }
+
+    lua_pop(L, 1);
 }
 
 #endif  /* NGX_HTTP_SSL */

src/ngx_http_lua_socket_tcp.h
@@ -147,6 +147,7 @@ typedef struct {
 
 void ngx_http_lua_inject_socket_tcp_api(ngx_log_t *log, lua_State *L);
 void ngx_http_lua_inject_req_socket_api(lua_State *L);
+void ngx_http_lua_cleanup_conn_pools(lua_State *L);
 
 
 #endif /* _NGX_HTTP_LUA_SOCKET_TCP_H_INCLUDED_ */

src/ngx_http_lua_util.c
@@ -48,6 +48,7 @@
 #include "ngx_http_lua_timer.h"
 #include "ngx_http_lua_config.h"
 #include "ngx_http_lua_worker.h"
+#include "ngx_http_lua_socket_tcp.h"
 
 
 #if 1
@@ -118,7 +119,6 @@ static ngx_int_t
     ngx_http_lua_ctx_t *ctx);
 static lua_State * ngx_http_lua_new_state(lua_State *parent_vm,
     ngx_cycle_t *cycle, ngx_http_lua_main_conf_t *lmcf, ngx_log_t *log);
-static void ngx_http_lua_cleanup_conn_pools(lua_State *L);
 static int ngx_http_lua_get_raw_phase_context(lua_State *L);
 
 
@@ -3737,41 +3737,6 @@ ngx_http_lua_cleanup_vm(void *data)
             ngx_free(state);
         }
     }
-}
-
-
-static void
-ngx_http_lua_cleanup_conn_pools(lua_State *L)
-{
-    ngx_queue_t                         *q;
-    ngx_connection_t                    *c;
-    ngx_http_lua_socket_pool_t          *spool;
-    ngx_http_lua_socket_pool_item_t     *item;
-
-    lua_pushlightuserdata(L, &ngx_http_lua_socket_pool_key);
-    lua_rawget(L, LUA_REGISTRYINDEX); /* table */
-
-    lua_pushnil(L);  /* first key */
-    while (lua_next(L, -2) != 0) {
-        /* tb key val */
-        spool = lua_touserdata(L, -1);
-
-        if (!ngx_queue_empty(&spool->cache)) {
-            q = ngx_queue_head(&spool->cache);
-            item = ngx_queue_data(q, ngx_http_lua_socket_pool_item_t, queue);
-            c = item->connection;
-            ngx_close_connection(c);
-            ngx_queue_remove(q);
-
-            ngx_log_debug1(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
-                           "lua tcp socket keepalive: free connection pool "
-                           "for \"%s\"", spool->key);
-        }
-
-        lua_pop(L, 1);
-    }
-
-    lua_pop(L, 1);
 }
 
 

commit e7d89c043588c7475ccb772d58f625657bc5276e

bugfix: builds without --with-http_ssl_module were broken. this regression had appeared in commit a6a0ed5.

src/ngx_http_lua_socket_tcp.c
@@ -5185,6 +5185,8 @@ ngx_http_lua_ssl_free_session(lua_State *L)
     return 0;
 }
 
+#endif  /* NGX_HTTP_SSL */
+
 
 void
 ngx_http_lua_cleanup_conn_pools(lua_State *L)
@@ -5221,7 +5223,5 @@ ngx_http_lua_cleanup_conn_pools(lua_State *L)
 
     lua_pop(L, 1);
 }
-
-#endif  /* NGX_HTTP_SSL */
 
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */