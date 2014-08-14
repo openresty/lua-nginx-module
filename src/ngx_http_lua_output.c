openresty/lua-nginx-module

commit 109fd454a18343eb77552cf82e870cf1d9a44af5

use lua_pushliteral for string-literals.

src/ngx_http_lua_output.c
@@ -294,7 +294,7 @@ ngx_http_lua_calc_strlen_in_table(lua_State *L, int index, int arg_i,
         /* not an array (non positive integer key) */
         lua_pop(L, 2); /* stack: table */
 
-        msg = lua_pushfstring(L, "non-array table found");
+        msg = lua_pushliteral(L, "non-array table found");
         luaL_argerror(L, arg_i, msg);
         return 0;
     }

src/ngx_http_lua_util.c
@@ -2801,7 +2801,7 @@ ngx_http_lua_thread_traceback(lua_State *L, lua_State *co,
 
             } else {
                 if (*ar.what == 'm') {  /* main? */
-                    lua_pushfstring(L, " in main chunk");
+                    lua_pushliteral(L, " in main chunk");
 
                 } else if (*ar.what == 'C' || *ar.what == 't') {
                     lua_pushliteral(L, " ?");  /* C function or tail call */

commit 90bdd9744ba2b37facec0c08c57dd8e36f643daf

fixed the build failure introduced in commit 109fd454a.

src/ngx_http_lua_output.c
@@ -294,8 +294,7 @@ ngx_http_lua_calc_strlen_in_table(lua_State *L, int index, int arg_i,
         /* not an array (non positive integer key) */
         lua_pop(L, 2); /* stack: table */
 
-        msg = lua_pushliteral(L, "non-array table found");
-        luaL_argerror(L, arg_i, msg);
+        luaL_argerror(L, arg_i, "non-array table found");
         return 0;
     }
 