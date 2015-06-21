openresty/lua-nginx-module

commit 9e0503b0358ee87719c7d8d990b6783d83c4e351

bugfix: raw downstream cosockets did not support full-deplexing. thanks aviramc for the bug report in #478 and the original patch in #481.

src/ngx_http_lua_common.h
@@ -339,6 +339,11 @@ typedef struct {
 } ngx_http_lua_vm_state_t;
 
 
+struct ngx_http_lua_socket_tcp_upstream_s;
+typedef struct ngx_http_lua_socket_tcp_upstream_s
+    ngx_http_lua_socket_tcp_upstream_t;
+
+
 typedef struct ngx_http_lua_ctx_s {
     /* for lua_coce_cache off: */
     ngx_http_lua_vm_state_t  *vm_state;
@@ -382,8 +387,10 @@ typedef struct ngx_http_lua_ctx_s {
 
     ngx_int_t                exit_code;
 
-    ngx_http_lua_co_ctx_t   *downstream_co_ctx; /* co ctx for the coroutine
-                                                   reading the request body */
+    union {
+        ngx_http_lua_co_ctx_t                  *co_ctx;
+        ngx_http_lua_socket_tcp_upstream_t     *u;
+    } downstream;
 
     ngx_uint_t               index;              /* index of the current
                                                     subrequest in its parent

src/ngx_http_lua_req_body.c
@@ -142,7 +142,7 @@ ngx_http_lua_ngx_req_read_body(lua_State *L)
                        "interruptions");
 
         ctx->waiting_more_body = 1;
-        ctx->downstream_co_ctx = coctx;
+        ctx->downstream.co_ctx = coctx;
 
         ngx_http_lua_cleanup_pending_operation(coctx);
         coctx->cleanup = ngx_http_lua_req_body_cleanup;
@@ -176,7 +176,7 @@ ngx_http_lua_req_body_post_read(ngx_http_request_t *r)
     if (ctx->waiting_more_body) {
         ctx->waiting_more_body = 0;
 
-        coctx = ctx->downstream_co_ctx;
+        coctx = ctx->downstream.co_ctx;
         ctx->cur_co_ctx = coctx;
 
         coctx->cleanup = NULL;

src/ngx_http_lua_socket_tcp.c
@@ -1842,7 +1842,7 @@ ngx_http_lua_socket_tcp_receive(lua_State *L)
     dd("setting data to %p, coctx:%p", u, coctx);
 
     if (u->raw_downstream || u->body_downstream) {
-        ctx->downstream_co_ctx = coctx;
+        ctx->downstream.u = u;
     }
 
     return lua_yield(L, 0);
@@ -3722,7 +3722,7 @@ ngx_http_lua_socket_receiveuntil_iterator(lua_State *L)
     dd("setting data to %p", u);
 
     if (u->raw_downstream || u->body_downstream) {
-        ctx->downstream_co_ctx = coctx;
+        ctx->downstream.u = u;
     }
 
     return lua_yield(L, 0);
@@ -4267,7 +4267,7 @@ ngx_http_lua_req_socket(lua_State *L)
     dd("setting data to %p", u);
 
     coctx->data = u;
-    ctx->downstream_co_ctx = coctx;
+    ctx->downstream.u = u;
 
     if (c->read->timer_set) {
         ngx_del_timer(c->read);
@@ -4299,9 +4299,7 @@ ngx_http_lua_req_socket_rev_handler(ngx_http_request_t *r)
         return;
     }
 
-    coctx = ctx->downstream_co_ctx;
-    u = coctx->data;
-
+    u = ctx->downstream.u;
     if (u) {
         u->read_event_handler(r, u);
     }

src/ngx_http_lua_util.c
@@ -1551,12 +1551,7 @@ ngx_http_lua_wev_handler(ngx_http_request_t *r)
     if (ctx->writing_raw_req_socket) {
         ctx->writing_raw_req_socket = 0;
 
-        coctx = ctx->downstream_co_ctx;
-        if (coctx == NULL) {
-            return NGX_ERROR;
-        }
-
-        u = coctx->data;
+        u = ctx->downstream.u;
         if (u == NULL) {
             return NGX_ERROR;
         }

t/131-duplex-req-socket.t
@@ -1,0 +1,141 @@
+# vim:set ft= ts=4 sw=4 et fdm=marker:
+
+BEGIN {
+    if (!defined $ENV{LD_PRELOAD}) {
+        $ENV{LD_PRELOAD} = '';
+    }
+
+    if ($ENV{LD_PRELOAD} !~ /\bmockeagain\.so\b/) {
+        $ENV{LD_PRELOAD} = "mockeagain.so $ENV{LD_PRELOAD}";
+    }
+
+    if ($ENV{MOCKEAGAIN} eq 'r') {
+        $ENV{MOCKEAGAIN} = 'rw';
+
+    } else {
+        $ENV{MOCKEAGAIN} = 'w';
+    }
+
+    $ENV{TEST_NGINX_EVENT_TYPE} = 'poll';
+    $ENV{MOCKEAGAIN_WRITE_TIMEOUT_PATTERN} = 'slow';
+}
+
+use Test::Nginx::Socket::Lua;
+
+log_level('debug');
+
+repeat_each(2);
+
+plan tests => repeat_each() * (blocks() * 2);
+
+run_tests();
+
+__DATA__
+
+=== TEST 1: raw downstream cosocket used in two different threads. See issue #481
+--- config
+    lua_socket_read_timeout 1ms;
+    lua_socket_send_timeout 1s;
+    lua_socket_log_errors off;
+
+    location /t {
+        content_by_lua '
+            local function reader(req_socket)
+               -- First we receive in a blocking fashion so that ctx->downstream_co_ctx will be changed
+               local data, err, partial = req_socket:receive(1)
+               if err ~= "timeout" then
+                  ngx.log(ngx.ERR, "Did not get timeout in the receiving thread!")
+                  return
+               end
+
+               -- Now, sleep so that coctx->data is changed to sleep handler
+               ngx.sleep(1)
+            end
+
+            local function writer(req_socket)
+               -- send in a slow manner with a low timeout, so that the timeout handler will be
+               local bytes, err = req_socket:send("slow!!!")
+               if err ~= "timeout" then
+                  return error("Did not get timeout in the sending thread!")
+               end
+            end
+
+            local req_socket, err = ngx.req.socket(true)
+            if req_socket == nil then
+               ngx.status = 500
+               return error("Unable to get request socket:" .. (err or "nil"))
+            end
+
+            local writer_thread = ngx.thread.spawn(writer, req_socket)
+            local reader_thread = ngx.thread.spawn(reader, req_socket)
+
+            ngx.thread.wait(writer_thread)
+            ngx.thread.wait(reader_thread)
+            print("The two threads finished")
+';
+        }
+--- request
+POST /t
+--- more_headers
+Content-Length: 1
+--- no_error_log
+[error]
+--- error_log: The two threads finished
+--- wait: 0.1
+--- ignore_response
+--- timeout: 10
+
+
+
+=== TEST 2: normal downstream cosocket used in two different threads. See issue #481
+--- config
+    lua_socket_read_timeout 1ms;
+    lua_socket_send_timeout 1s;
+    lua_socket_log_errors off;
+    send_timeout 1s;
+
+    location /t {
+        content_by_lua '
+            local function reader(req_socket)
+               -- First we receive in a blocking fashion so that ctx->downstream_co_ctx will be changed
+               local data, err, partial = req_socket:receive(1)
+               if err ~= "timeout" then
+                  ngx.log(ngx.ERR, "Did not get timeout in the receiving thread!")
+                  return
+               end
+
+               -- Now, sleep so that coctx->data is changed to sleep handler
+               ngx.sleep(1)
+            end
+
+            local function writer(req_socket)
+               -- send in a slow manner with a low timeout, so that the timeout handler will be
+               ngx.sleep(0.3)
+               ngx.say("slow!!!")
+               ngx.flush(true)
+            end
+
+            local req_socket, err = ngx.req.socket()
+            if req_socket == nil then
+               ngx.status = 500
+               return error("Unable to get request socket:" .. (err or "nil"))
+            end
+
+            local writer_thread = ngx.thread.spawn(writer, req_socket)
+            local reader_thread = ngx.thread.spawn(reader, req_socket)
+
+            ngx.thread.wait(writer_thread)
+            ngx.thread.wait(reader_thread)
+            print("The two threads finished")
+';
+        }
+--- request
+POST /t
+--- more_headers
+Content-Length: 1
+--- no_error_log
+[error]
+--- error_log: The two threads finished
+--- wait: 0.1
+--- ignore_response
+--- timeout: 10

commit 721fff24fcb528e5aa5609c82825dbce987b990d

bugfix: fixed compiler warnings at least with gcc 4.7 and clang 3.5. this regression had appeared in commit 9e0503b035.

src/ngx_http_lua_common.h
@@ -339,11 +339,6 @@ typedef struct {
 } ngx_http_lua_vm_state_t;
 
 
-struct ngx_http_lua_socket_tcp_upstream_s;
-typedef struct ngx_http_lua_socket_tcp_upstream_s
-    ngx_http_lua_socket_tcp_upstream_t;
-
-
 typedef struct ngx_http_lua_ctx_s {
     /* for lua_coce_cache off: */
     ngx_http_lua_vm_state_t  *vm_state;
@@ -387,10 +382,9 @@ typedef struct ngx_http_lua_ctx_s {
 
     ngx_int_t                exit_code;
 
-    union {
-        ngx_http_lua_co_ctx_t                  *co_ctx;
-        ngx_http_lua_socket_tcp_upstream_t     *u;
-    } downstream;
+    void                    *downstream;  /* can be either
+                                             ngx_http_lua_socket_tcp_upstream_t
+                                             or ngx_http_lua_co_ctx_t */
 
     ngx_uint_t               index;              /* index of the current
                                                     subrequest in its parent

src/ngx_http_lua_req_body.c
@@ -142,7 +142,7 @@ ngx_http_lua_ngx_req_read_body(lua_State *L)
                        "interruptions");
 
         ctx->waiting_more_body = 1;
-        ctx->downstream.co_ctx = coctx;
+        ctx->downstream = coctx;
 
         ngx_http_lua_cleanup_pending_operation(coctx);
         coctx->cleanup = ngx_http_lua_req_body_cleanup;
@@ -176,7 +176,7 @@ ngx_http_lua_req_body_post_read(ngx_http_request_t *r)
     if (ctx->waiting_more_body) {
         ctx->waiting_more_body = 0;
 
-        coctx = ctx->downstream.co_ctx;
+        coctx = ctx->downstream;
         ctx->cur_co_ctx = coctx;
 
         coctx->cleanup = NULL;

src/ngx_http_lua_socket_tcp.c
@@ -1842,7 +1842,7 @@ ngx_http_lua_socket_tcp_receive(lua_State *L)
     dd("setting data to %p, coctx:%p", u, coctx);
 
     if (u->raw_downstream || u->body_downstream) {
-        ctx->downstream.u = u;
+        ctx->downstream = u;
     }
 
     return lua_yield(L, 0);
@@ -3722,7 +3722,7 @@ ngx_http_lua_socket_receiveuntil_iterator(lua_State *L)
     dd("setting data to %p", u);
 
     if (u->raw_downstream || u->body_downstream) {
-        ctx->downstream.u = u;
+        ctx->downstream = u;
     }
 
     return lua_yield(L, 0);
@@ -4267,7 +4267,7 @@ ngx_http_lua_req_socket(lua_State *L)
     dd("setting data to %p", u);
 
     coctx->data = u;
-    ctx->downstream.u = u;
+    ctx->downstream = u;
 
     if (c->read->timer_set) {
         ngx_del_timer(c->read);
@@ -4288,7 +4288,6 @@ static void
 ngx_http_lua_req_socket_rev_handler(ngx_http_request_t *r)
 {
     ngx_http_lua_ctx_t                  *ctx;
-    ngx_http_lua_co_ctx_t               *coctx;
     ngx_http_lua_socket_tcp_upstream_t  *u;
 
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
@@ -4299,7 +4298,7 @@ ngx_http_lua_req_socket_rev_handler(ngx_http_request_t *r)
         return;
     }
 
-    u = ctx->downstream.u;
+    u = ctx->downstream;
     if (u) {
         u->read_event_handler(r, u);
     }

src/ngx_http_lua_util.c
@@ -1500,7 +1500,6 @@ ngx_http_lua_wev_handler(ngx_http_request_t *r)
     ngx_event_t                 *wev;
     ngx_connection_t            *c;
     ngx_http_lua_ctx_t          *ctx;
-    ngx_http_lua_co_ctx_t       *coctx;
     ngx_http_core_loc_conf_t    *clcf;
 
     ngx_http_lua_socket_tcp_upstream_t *u;
@@ -1551,7 +1550,7 @@ ngx_http_lua_wev_handler(ngx_http_request_t *r)
     if (ctx->writing_raw_req_socket) {
         ctx->writing_raw_req_socket = 0;
 
-        u = ctx->downstream.u;
+        u = ctx->downstream;
         if (u == NULL) {
             return NGX_ERROR;
         }