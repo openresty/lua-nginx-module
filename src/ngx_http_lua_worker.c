ea16067fe387293a629f469c5bff71a90d025ac6
YuanSheng Wang <membphis@gmail.com>
2015-10-12 08:02:04 +0800

    add new ngx.worker.count api
    add the test case later

--- src/ngx_http_lua_worker.c
+++ src/ngx_http_lua_worker.c
@@ -15,18 +15,22 @@
 
 static int ngx_http_lua_ngx_worker_exiting(lua_State *L);
 static int ngx_http_lua_ngx_worker_pid(lua_State *L);
+static int ngx_http_lua_ngx_worker_count(lua_State *L);
 
 
 void
 ngx_http_lua_inject_worker_api(lua_State *L)
 {
-    lua_createtable(L, 0 /* narr */, 2 /* nrec */);    /* ngx.worker. */
+    lua_createtable(L, 0 /* narr */, 3 /* nrec */);    /* ngx.worker. */
 
     lua_pushcfunction(L, ngx_http_lua_ngx_worker_exiting);
     lua_setfield(L, -2, "exiting");
 
     lua_pushcfunction(L, ngx_http_lua_ngx_worker_pid);
     lua_setfield(L, -2, "pid");
+
+    lua_pushcfunction(L, ngx_http_lua_ngx_worker_count);
+    lua_setfield(L, -2, "count");
 
     lua_setfield(L, -2, "worker");
 }
@@ -60,5 +64,16 @@ int
 ngx_http_lua_ffi_worker_exiting(void)
 {
     return (int) ngx_exiting;
+}
+
+
+static int
+ngx_http_lua_ngx_worker_count(lua_State *L)
+{
+    ngx_core_conf_t   *ccf;
+    ccf = (ngx_core_conf_t *) ngx_get_conf(ngx_cycle->conf_ctx, ngx_core_module);
+
+    lua_pushinteger(L, ccf->worker_processes);
+    return 1;
 }
 #endif
