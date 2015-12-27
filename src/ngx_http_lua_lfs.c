141f4750684efa54a45c671f29847fb219da4409
anhk <anhk_@126.com>
2015-12-27 22:31:41 +0800

    add empty file

--- src/ngx_http_lua_lfs.c
+++ src/ngx_http_lua_lfs.c
@@ -4,4 +4,35 @@
  * Copyright (C) Terry AN (anhk)
  **/
 
+#include "ddebug.h"
+#include "ngx_http_lua_util.h"
 #include "ngx_http_lua_lfs.h"
+
+#if (NGX_THREADS)
+
+static int ngx_http_lua_ngx_lfs_read(lua_State *L)
+{
+    lua_pushlstring(L, "From LFS", strlen("From LFS"));
+    return 1;
+}
+
+static int ngx_http_lua_ngx_lfs_write(lua_State *L)
+{
+    return 0;
+}
+
+void ngx_http_lua_inject_lfs_api(lua_State *L)
+{
+    lua_createtable(L, 0 /* narr */, 2 /* nrec */);    /* ngx.lfs. */
+
+    lua_pushcfunction(L, ngx_http_lua_ngx_lfs_read);
+    lua_setfield(L, -2, "read");
+
+    lua_pushcfunction(L, ngx_http_lua_ngx_lfs_write);
+    lua_setfield(L, -2, "write");
+
+    lua_setfield(L, -2, "lfs");
+}
+
+
+#endif
--- src/ngx_http_lua_lfs.h
+++ src/ngx_http_lua_lfs.h
@@ -8,5 +8,8 @@
 #define _NGX_HTTP_LUA_LFS_H_INCLUDED_
 
 
+#if (NGX_THREADS)
+void ngx_http_lua_inject_lfs_api(lua_State *L);
+#endif
 
 #endif
