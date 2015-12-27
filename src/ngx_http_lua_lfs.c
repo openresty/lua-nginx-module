6d4b80b3fad704557d88781d598fa101c6083282
anhk <anhk_@126.com>
2015-12-27 22:37:44 +0800

    add some api

--- src/ngx_http_lua_lfs.c
+++ src/ngx_http_lua_lfs.c
@@ -21,15 +21,39 @@ static int ngx_http_lua_ngx_lfs_write(lua_State *L)
     return 0;
 }
 
+static int ngx_http_lua_ngx_lfs_copy(lua_State *L)
+{
+    return 0;
+}
+
+static int ngx_http_lua_ngx_lfs_unlink(lua_State *L)
+{
+    return 0;
+}
+
+static int ngx_http_lua_ngx_lfs_status(lua_State *L)
+{
+    return 0;
+}
+
 void ngx_http_lua_inject_lfs_api(lua_State *L)
 {
-    lua_createtable(L, 0 /* narr */, 2 /* nrec */);    /* ngx.lfs. */
+    lua_createtable(L, 0 /* narr */, 5 /* nrec */);    /* ngx.lfs. */
 
     lua_pushcfunction(L, ngx_http_lua_ngx_lfs_read);
     lua_setfield(L, -2, "read");
 
     lua_pushcfunction(L, ngx_http_lua_ngx_lfs_write);
     lua_setfield(L, -2, "write");
+
+    lua_pushcfunction(L, ngx_http_lua_ngx_lfs_copy);
+    lua_setfield(L, -2, "copy");
+
+    lua_pushcfunction(L, ngx_http_lua_ngx_lfs_unlink);
+    lua_setfield(L, -2, "unlink");
+
+    lua_pushcfunction(L, ngx_http_lua_ngx_lfs_status);
+    lua_setfield(L, -2, "status");
 
     lua_setfield(L, -2, "lfs");
 }
