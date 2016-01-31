openresty/lua-nginx-module

commit cfd4f904d4776dfcfefd460c643775a30efd2042

change: unmatched captures are set to false in the captures table.
Now unmatched capture groups in `ngx.re.match`, `ngx.re.gmatch`,
`ngx.re.sub`, and `ngx.re.gsub` are
set to `false` rather than nil. This prevents to create tables with
holes that don't play nicely with `ipairs` or `#` operators.

For consistency, unmatched trailing captures (for instance in
`ngx.re.match("hello", "(hello)(.+)?")`) are
also set to `false`.

Named captures are not affected.

src/ngx_http_lua_regex.c
@@ -592,14 +592,14 @@ exec:
     }
 
     if (res_tb_idx == 0) {
-        lua_createtable(L, rc /* narr */, 0 /* nrec */);
+        lua_createtable(L, re_comp.captures /* narr */, name_count /* nrec */);
         res_tb_idx = lua_gettop(L);
     }
 
-    for (i = 0, n = 0; i < rc; i++, n += 2) {
+    for (i = 0, n = 0; i <= re_comp.captures; i++, n += 2) {
         dd("capture %d: %d %d", i, cap[n], cap[n + 1]);
-        if (cap[n] < 0) {
-            lua_pushnil(L);
+        if (i >= rc || cap[n] < 0) {
+            lua_pushboolean(L, 0);
 
         } else {
             lua_pushlstring(L, (char *) &subj.data[cap[n]],
@@ -1116,12 +1116,12 @@ ngx_http_lua_ngx_re_gmatch_iterator(lua_State *L)
 
     dd("rc = %d", (int) rc);
 
-    lua_createtable(L, rc /* narr */, 0 /* nrec */);
+    lua_createtable(L, ctx->ncaptures /* narr */, name_count /* nrec */);
 
-    for (i = 0, n = 0; i < rc; i++, n += 2) {
+    for (i = 0, n = 0; i <= ctx->ncaptures; i++, n += 2) {
         dd("capture %d: %d %d", i, cap[n], cap[n + 1]);
-        if (cap[n] < 0) {
-            lua_pushnil(L);
+        if (i >= rc || cap[n] < 0) {
+            lua_pushboolean(L, 0);
 
         } else {
             lua_pushlstring(L, (char *) &subj.data[cap[n]],
@@ -1750,12 +1750,12 @@ exec:
         if (func) {
             lua_pushvalue(L, 3);
 
-            lua_createtable(L, rc - 1 /* narr */, 1 /* nrec */);
+            lua_createtable(L, re_comp.captures /* narr */, name_count /* nrec */);
 
-            for (i = 0, n = 0; i < rc; i++, n += 2) {
+            for (i = 0, n = 0; i <= re_comp.captures; i++, n += 2) {
                 dd("capture %d: %d %d", (int) i, cap[n], cap[n + 1]);
-                if (cap[n] < 0) {
-                    lua_pushnil(L);
+                if (i >= rc || cap[n] < 0) {
+                    lua_pushboolean(L, 0);
 
                 } else {
                     lua_pushlstring(L, (char *) &subj.data[cap[n]],
@@ -2071,6 +2071,11 @@ ngx_http_lua_re_collect_named_captures(lua_State *L, int res_tb_idx,
         }
 
         if (flags & NGX_LUA_RE_MODE_DUPNAMES) {
+            /* unmatched groups are not stored in tables in DUPNAMES mode */
+            if (!lua_toboolean(L, -1)) {
+                lua_pop(L, 1);
+                continue;
+            }
 
             lua_getfield(L, -2, name); /* big_tb cap small_tb */
 

README.markdown
@@ -861,7 +861,6 @@ TODO
 * cosocket: pool-based backend concurrency level control: implement automatic `connect` queueing when the backend concurrency exceeds its connection pool limit.
 * cosocket: review and merge aviramc's [patch](https://github.com/openresty/lua-nginx-module/pull/290) for adding the `bsdrecv` method.
 * add new API function `ngx.resp.add_header` to emulate the standard `add_header` config directive.
-* [ngx.re](#ngxrematch) API: use `false` instead of `nil` in the resulting match table to indicate non-existent submatch captures, such that we can avoid "holes" in the array table.
 * review and apply Jader H. Silva's patch for `ngx.re.split()`.
 * review and apply vadim-pavlov's patch for [ngx.location.capture](#ngxlocationcapture)'s `extra_headers` option
 * use `ngx_hash_t` to optimize the built-in header look-up process for [ngx.req.set_header](#ngxreqset_header), [ngx.header.HEADER](#ngxheaderheader), and etc.
@@ -5524,16 +5523,16 @@ and are returned in the same Lua table as key-value pairs as the numbered captur
  -- m["remaining"] == "234"
 ```
 
-Unmatched subpatterns will have `nil` values in their `captures` table fields.
+Unmatched subpatterns will have `false` values in their `captures` table fields.
 
 ```lua
 
  local m, err = ngx.re.match("hello, world", "(world)|(hello)|(?<named>howdy)")
  -- m[0] == "hello"
- -- m[1] == nil
+ -- m[1] == false
  -- m[2] == "hello"
- -- m[3] == nil
- -- m["named"] == nil
+ -- m[3] == false
+ -- m["named"] == false
 ```
 
 Specify `options` to control how the match operation will be performed. The following option characters are supported:

doc/HttpLuaModule.wiki
@@ -698,7 +698,6 @@ phases.
 * cosocket: pool-based backend concurrency level control: implement automatic <code>connect</code> queueing when the backend concurrency exceeds its connection pool limit.
 * cosocket: review and merge aviramc's [https://github.com/openresty/lua-nginx-module/pull/290 patch] for adding the <code>bsdrecv</code> method.
 * add new API function <code>ngx.resp.add_header</code> to emulate the standard <code>add_header</code> config directive.
-* [[#ngx.re.match|ngx.re]] API: use <code>false</code> instead of <code>nil</code> in the resulting match table to indicate non-existent submatch captures, such that we can avoid "holes" in the array table.
 * review and apply Jader H. Silva's patch for <code>ngx.re.split()</code>.
 * review and apply vadim-pavlov's patch for [[#ngx.location.capture|ngx.location.capture]]'s <code>extra_headers</code> option
 * use <code>ngx_hash_t</code> to optimize the built-in header look-up process for [[#ngx.req.set_header|ngx.req.set_header]], [[#ngx.header.HEADER|ngx.header.HEADER]], and etc.
@@ -4593,15 +4592,15 @@ and are returned in the same Lua table as key-value pairs as the numbered captur
     -- m["remaining"] == "234"
 </geshi>
 
-Unmatched subpatterns will have <code>nil</code> values in their <code>captures</code> table fields.
+Unmatched subpatterns will have <code>false</code> values in their <code>captures</code> table fields.
 
 <geshi lang="lua">
     local m, err = ngx.re.match("hello, world", "(world)|(hello)|(?<named>howdy)")
     -- m[0] == "hello"
-    -- m[1] == nil
+    -- m[1] == false
     -- m[2] == "hello"
-    -- m[3] == nil
-    -- m["named"] == nil
+    -- m[3] == false
+    -- m["named"] == false
 </geshi>
 
 Specify <code>options</code> to control how the match operation will be performed. The following option characters are supported:

t/034-match.t
@@ -409,7 +409,7 @@ error: .*?unknown flag "H" \(flags "Hm"\)
     GET /re
 --- response_body
 hello
-nil
+false
 hello
 
 
@@ -814,7 +814,7 @@ hello-1234
 
 
 
-=== TEST 38: named captures are nil
+=== TEST 38: named captures are false
 --- config
     location /re {
         content_by_lua '
@@ -834,10 +834,10 @@ hello-1234
     GET /re
 --- response_body
 hello
-nil
+false
 hello
-nil
-nil
+false
+false
 
 
 
@@ -1147,4 +1147,25 @@ failed to match
 --- response_body
 1234
 --- SKIP
+
+
+
+=== TEST 49: trailing captures are false
+--- config
+    location /re {
+        content_by_lua '
+            local m = ngx.re.match("hello", "(hello)(.+)?")
+            if m then
+                ngx.say(m[0])
+                ngx.say(m[1])
+                ngx.say(m[2])
+            end
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hello
+hello
+false
 

t/035-gmatch.t
@@ -582,13 +582,13 @@ matched: []
 --- response_body
 1234
 1234
-nil
+false
 1234
-nil
+false
 abcd
-nil
+false
 abcd
-nil
+false
 abcd
 
 

t/036-sub.t
@@ -8,7 +8,7 @@ use Test::Nginx::Socket::Lua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 2 + 18);
+plan tests => repeat_each() * (blocks() * 2 + 19);
 
 #no_diff();
 no_long_string();
@@ -732,3 +732,26 @@ GET /t
 bad argument type
 NYI
 
+
+
+=== TEST 33: function replace (false for groups)
+--- config
+    location /re {
+        content_by_lua '
+            local repl = function (m)
+                print("group 1: ", m[2])
+                return "[" .. m[0] .. "] [" .. m[1] .. "]"
+            end
+
+            local s, n = ngx.re.sub("hello, 34", "([0-9])|(world)", repl)
+            ngx.say(s)
+            ngx.say(n)
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hello, [3] [3]4
+1
+--- error_log
+group 1: false

t/037-gsub.t
@@ -4,11 +4,11 @@ use Test::Nginx::Socket::Lua;
 #worker_connections(1014);
 #master_on();
 #workers(2);
-log_level('warn');
+#log_level('warn');
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 2 + 16);
+plan tests => repeat_each() * (blocks() * 2 + 17);
 
 #no_diff();
 no_long_string();
@@ -674,3 +674,26 @@ GET /t
 --- no_error_log
 [error]
 
+
+
+=== TEST 29: function replace (false for groups)
+--- config
+    location /re {
+        content_by_lua '
+            local repl = function (m)
+                print("group 1: ", m[2])
+                return "[" .. m[0] .. "] [" .. m[1] .. "]"
+            end
+
+            local s, n = ngx.re.gsub("hello, 34", "([0-9])|(world)", repl)
+            ngx.say(s)
+            ngx.say(n)
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hello, [3] [3][4] [4]
+2
+--- error_log
+group 1: false

t/038-match-o.t
@@ -384,7 +384,7 @@ error: pcre_compile() failed: missing ) in "(abc"
     GET /re
 --- response_body
 hello
-nil
+false
 hello
 
 
@@ -716,7 +716,7 @@ hello-1234
 
 
 
-=== TEST 33: named captures are nil
+=== TEST 33: named captures are false
 --- config
     location /re {
         content_by_lua '
@@ -736,8 +736,8 @@ hello-1234
     GET /re
 --- response_body
 hello
-nil
+false
 hello
-nil
-nil
+false
+false
 

commit a43bc0a1b1bb1b2fc2983cf03a191d92b1406219

bugfix: ngx.re: memory over-reads might happen in DFA mode when the regex has submatch capturing groups.
This regression had appeared in commit cfd4f904d4.

src/ngx_http_lua_regex.c
@@ -420,6 +420,7 @@ ngx_http_lua_ngx_re_match_helper(lua_State *L, int wantcaps)
 
     if (flags & NGX_LUA_RE_MODE_DFA) {
         ovecsize = 2;
+        re_comp.captures = 0;
 
     } else {
         ovecsize = (re_comp.captures + 1) * 3;
@@ -882,6 +883,7 @@ ngx_http_lua_ngx_re_gmatch(lua_State *L)
 
     if (flags & NGX_LUA_RE_MODE_DFA) {
         ovecsize = 2;
+        re_comp.captures = 0;
 
     } else {
         ovecsize = (re_comp.captures + 1) * 3;
@@ -1565,6 +1567,7 @@ ngx_http_lua_ngx_re_sub_helper(lua_State *L, unsigned global)
 
     if (flags & NGX_LUA_RE_MODE_DFA) {
         ovecsize = 2;
+        re_comp.captures = 0;
 
     } else {
         ovecsize = (re_comp.captures + 1) * 3;
@@ -2208,6 +2211,7 @@ ngx_http_lua_ffi_compile_regex(const unsigned char *pat, size_t pat_len,
 
     if (flags & NGX_LUA_RE_MODE_DFA) {
         ovecsize = 2;
+        re_comp.captures = 0;
 
     } else {
         ovecsize = (re_comp.captures + 1) * 3;
@@ -2286,6 +2290,7 @@ ngx_http_lua_ffi_exec_regex(ngx_http_lua_regex_t *re, int flags,
 
     if (flags & NGX_LUA_RE_MODE_DFA) {
         ovecsize = 2;
+        re->ncaptures = 0;
 
     } else {
         ovecsize = (re->ncaptures + 1) * 3;

t/048-match-dfa.t
@@ -23,6 +23,8 @@ __DATA__
             m = ngx.re.match("hello", "(he|hell)", "d")
             if m then
                 ngx.say(m[0])
+                ngx.say(m[1])
+                ngx.say(m[2])
             else
                 ngx.say("not matched!")
             end
@@ -32,10 +34,35 @@ __DATA__
     GET /re
 --- response_body
 hell
+nil
+nil
 
 
 
-=== TEST 2: matched with d + j
+=== TEST 2: matched with d + o
+--- config
+    location /re {
+        content_by_lua '
+            m = ngx.re.match("hello", "(he|hell)", "do")
+            if m then
+                ngx.say(m[0])
+                ngx.say(m[1])
+                ngx.say(m[2])
+            else
+                ngx.say("not matched!")
+            end
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hell
+nil
+nil
+
+
+
+=== TEST 3: matched with d + j
 --- config
     location /re {
         content_by_lua '
@@ -54,7 +81,7 @@ hell
 
 
 
-=== TEST 3: not matched with j
+=== TEST 4: not matched with j
 --- config
     location /re {
         content_by_lua '
@@ -73,7 +100,7 @@ not matched!
 
 
 
-=== TEST 4: matched with do
+=== TEST 5: matched with do
 --- config
     location /re {
         content_by_lua '
@@ -96,7 +123,7 @@ nil
 
 
 
-=== TEST 5: not matched with do
+=== TEST 6: not matched with do
 --- config
     location /re {
         content_by_lua '
@@ -115,7 +142,7 @@ not matched!
 
 
 
-=== TEST 6: UTF-8 mode without UTF-8 sequence checks
+=== TEST 7: UTF-8 mode without UTF-8 sequence checks
 --- config
     location /re {
         content_by_lua '
@@ -149,7 +176,7 @@ exec opts: 2000
 
 
 
-=== TEST 7: UTF-8 mode with UTF-8 sequence checks
+=== TEST 8: UTF-8 mode with UTF-8 sequence checks
 --- config
     location /re {
         content_by_lua '

t/050-gmatch-dfa.t
@@ -23,6 +23,7 @@ __DATA__
             for m in ngx.re.gmatch("hello, halo", "h[a-z]|h[a-z][a-z]", "d") do
                 if m then
                     ngx.say(m[0])
+                    ngx.say(m[1])
                 else
                     ngx.say("not matched: ", m)
                 end
@@ -33,7 +34,9 @@ __DATA__
     GET /re
 --- response_body
 hel
+nil
 hal
+nil
 
 
 
@@ -286,3 +289,50 @@ exec opts: 0
 --- no_error_log
 [error]
 
+
+
+=== TEST 13: gmatched with submatch captures
+--- config
+    location /re {
+        content_by_lua '
+            for m in  ngx.re.gmatch("hello", "(he|hell)", "d") do
+                if m then
+                    ngx.say(m[0])
+                    ngx.say(m[1])
+                    ngx.say(m[2])
+                else
+                    ngx.say("not matched!")
+                end
+            end
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hell
+nil
+nil
+
+
+
+=== TEST 14: gmatched with submatch captures (compile once)
+--- config
+    location /re {
+        content_by_lua '
+            for m in  ngx.re.gmatch("hello", "(he|hell)", "od") do
+                if m then
+                    ngx.say(m[0])
+                    ngx.say(m[1])
+                    ngx.say(m[2])
+                else
+                    ngx.say("not matched!")
+                end
+            end
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hell
+nil
+nil

t/052-sub-dfa.t
@@ -8,7 +8,7 @@ use Test::Nginx::Socket::Lua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 2 + 6);
+plan tests => repeat_each() * (blocks() * 2 + 8);
 
 #no_diff();
 no_long_string();
@@ -198,3 +198,38 @@ s: a好
 --- no_error_log
 [error]
 
+
+
+=== TEST 9: sub with d
+--- config
+    location /re {
+        content_by_lua '
+            ngx.say(ngx.re.sub("hello", "(he|hell)", function (m) ngx.say(m[0]) ngx.say(m[1]) return "x" end, "d"))
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hell
+nil
+xo1
+--- no_error_log
+[error]
+
+
+
+=== TEST 10: sub with d + o
+--- config
+    location /re {
+        content_by_lua '
+            ngx.say(ngx.re.sub("hello", "(he|hell)", function (m) ngx.say(m[0]) ngx.say(m[1]) return "x" end, "do"))
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hell
+nil
+xo1
+--- no_error_log
+[error]

t/054-gsub-dfa.t
@@ -8,7 +8,7 @@ use Test::Nginx::Socket::Lua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 2 + 5);
+plan tests => repeat_each() * (blocks() * 2 + 7);
 
 #no_diff();
 no_long_string();
@@ -199,3 +199,38 @@ s: aa
 --- no_error_log
 [error]
 
+
+
+=== TEST 9: gsub with d
+--- config
+    location /re {
+        content_by_lua '
+            ngx.say(ngx.re.gsub("hello", "(he|hell)", function (m) ngx.say(m[0]) ngx.say(m[1]) return "x" end, "d"))
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hell
+nil
+xo1
+--- no_error_log
+[error]
+
+
+
+=== TEST 10: gsub with d + o
+--- config
+    location /re {
+        content_by_lua '
+            ngx.say(ngx.re.gsub("hello", "(he|hell)", function (m) ngx.say(m[0]) ngx.say(m[1]) return "x" end, "do"))
+        ';
+    }
+--- request
+    GET /re
+--- response_body
+hell
+nil
+xo1
+--- no_error_log
+[error]