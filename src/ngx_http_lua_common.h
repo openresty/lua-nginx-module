83ff3e89c64b5cc90b3784389fd49ddb1c65bb51
tan <312841925@qq.com>
2017-03-08 23:23:34 +0800

    feature: allow use of ngx.exit() in the context of body_filter_by_lua

--- src/ngx_http_lua_common.h
+++ src/ngx_http_lua_common.h
@@ -559,6 +559,8 @@ typedef struct ngx_http_lua_ctx_s {
 
     unsigned         exited:1;
 
+    unsigned         body_filter_aborted:1;
+
     unsigned         eof:1;             /*  1: last_buf has been sent;
                                             0: last_buf not sent yet */
 
