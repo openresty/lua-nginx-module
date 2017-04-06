1a5a33e61f4d747efa8460555407357ca166eca1
Yuansheng <membphis@gmail.com>
2017-04-06 18:31:56 +0800

    feature: added new config directive lua_intercept_error_log for capturing nginx error logs on Lua land.
    The corresponding Lua API is provided by the ngx.errlog module in
    lua-resty-core.
    
    Signed-off-by: Yichun Zhang (agentzh) <agentzh@gmail.com>

--- src/ngx_http_lua_log.c
+++ src/ngx_http_lua_log.c
@@ -13,6 +13,7 @@
 
 #include "ngx_http_lua_log.h"
 #include "ngx_http_lua_util.h"
+#include "ngx_http_lua_log_ringbuf.h"
 
 
 static int ngx_http_lua_print(lua_State *L);
@@ -312,5 +313,100 @@ ngx_http_lua_inject_log_consts(lua_State *L)
     lua_setfield(L, -2, "DEBUG");
     /* }}} */
 }
+
+
+#ifdef HAVE_INTERCEPT_ERROR_LOG_PATCH
+ngx_int_t
+ngx_http_lua_intercept_log_handler(ngx_log_t *log,
+    ngx_uint_t level, void *buf, size_t n)
+{
+    ngx_http_lua_log_ringbuf_t  *ringbuf;
+
+    dd("enter");
+
+    ringbuf = (ngx_http_lua_log_ringbuf_t  *)
+                    ngx_cycle->intercept_error_log_data;
+
+    if (level > ringbuf->filter_level) {
+        return NGX_OK;
+    }
+
+    ngx_http_lua_log_ringbuf_write(ringbuf, level, buf, n);
+
+    dd("intercept log: %s\n", buf);
+
+    return NGX_OK;
+}
+#endif
+
+
+#ifndef NGX_LUA_NO_FFI_API
+int
+ngx_http_lua_ffi_set_errlog_filter(int level, u_char *err, size_t *errlen)
+{
+#ifdef HAVE_INTERCEPT_ERROR_LOG_PATCH
+    ngx_http_lua_log_ringbuf_t     *ringbuf;
+
+    ringbuf = ngx_cycle->intercept_error_log_data;
+
+    if (ringbuf == NULL) {
+        *errlen = ngx_snprintf(err, *errlen,
+                               "API \"set_errlog_filter\" depends on "
+                               "directive \"lua_intercept_error_log\"")
+                  - err;
+        return NGX_ERROR;
+    }
+
+    if (level > NGX_LOG_DEBUG || level < NGX_LOG_STDERR) {
+        *errlen = ngx_snprintf(err, *errlen, "bad log level: %d", level)
+                  - err;
+        return NGX_ERROR;
+    }
+
+    ringbuf->filter_level = level;
+    return NGX_OK;
+#else
+    *errlen = ngx_snprintf(err, *errlen,
+                           "missing the intercept error log patch for nginx")
+              - err;
+    return NGX_ERROR;
+#endif
+}
+
+
+int
+ngx_http_lua_ffi_get_errlog_data(char **log, int *loglevel, u_char *err,
+    size_t *errlen)
+{
+#ifdef HAVE_INTERCEPT_ERROR_LOG_PATCH
+    ngx_uint_t           loglen;
+
+    ngx_http_lua_log_ringbuf_t     *ringbuf;
+
+    ringbuf = ngx_cycle->intercept_error_log_data;
+
+    if (ringbuf == NULL) {
+        *errlen = ngx_snprintf(err, *errlen,
+                               "API \"get_errlog_data\" depends on directive "
+                               "\"lua_intercept_error_log\"")
+                  - err;
+        return NGX_ERROR;
+    }
+
+    if (ringbuf->count == 0) {
+        return NGX_DONE;
+    }
+
+    ngx_http_lua_log_ringbuf_read(ringbuf, loglevel, (void **)log, &loglen);
+    return loglen;
+#else
+    *errlen = ngx_snprintf(err, *errlen,
+                           "missing the intercept error log patch for nginx")
+              - err;
+    return NGX_ERROR;
+#endif
+}
+
+#endif
 
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */
