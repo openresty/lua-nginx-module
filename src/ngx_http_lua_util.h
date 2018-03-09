809192c96cce68023ffd365363d064ee13f68600
spacewander <spacewanderlzx@gmail.com>
2018-03-09 16:00:41 +0800

    feature: api.h: exposed the ngx_http_lua_ffi_str_t type for other Nginx C modules.
    Signed-off-by: Yichun Zhang (agentzh) <agentzh@gmail.com>

--- src/ngx_http_lua_util.h
+++ src/ngx_http_lua_util.h
@@ -10,6 +10,7 @@
 
 
 #include "ngx_http_lua_common.h"
+#include "ngx_http_lua_api.h"
 
 
 #ifndef NGX_UNESCAPE_URI_COMPONENT
@@ -18,13 +19,6 @@
 
 
 #ifndef NGX_LUA_NO_FFI_API
-typedef struct {
-    int          len;
-    /* this padding hole on 64-bit systems is expected */
-    u_char      *data;
-} ngx_http_lua_ffi_str_t;
-
-
 typedef struct {
     ngx_http_lua_ffi_str_t   key;
     ngx_http_lua_ffi_str_t   value;
