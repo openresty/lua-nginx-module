c6108a04297bb53d99fbd398b9c80b72b5050a2e
Yichun Zhang (agentzh) <agentzh@gmail.com>
2018-04-20 15:22:47 -0700

    bugfix: fixed a minor issue found by spacewander.

--- src/ngx_http_lua_initworkerby.c
+++ src/ngx_http_lua_initworkerby.c
@@ -46,9 +46,15 @@ ngx_http_lua_init_worker(ngx_cycle_t *cycle)
 
     /* lmcf != NULL && lmcf->lua != NULL */
 
-    /* disable init_worker_by_lua* and destroy lua VM in cache processes */
-#if defined(HAVE_PRIVILEGED_PROCESS_PATCH) && !NGX_WIN32
-    if (ngx_process == NGX_PROCESS_HELPER && !ngx_is_privileged_agent) {
+#if !(NGX_WIN32)
+    if (ngx_process == NGX_PROCESS_HELPER
+#   ifdef HAVE_PRIVILEGED_PROCESS_PATCH
+        && !ngx_is_privileged_agent
+#   endif
+       )
+    {
+        /* disable init_worker_by_lua* and destroy lua VM in cache processes */
+
         ngx_log_debug2(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
                        "lua close the global Lua VM %p in the "
                        "cache helper process %P", lmcf->lua, ngx_pid);
@@ -58,7 +64,7 @@ ngx_http_lua_init_worker(ngx_cycle_t *cycle)
 
         return NGX_OK;
     }
-#endif
+#endif  /* NGX_WIN32 */
 
     if (lmcf->init_worker_handler == NULL) {
         return NGX_OK;
