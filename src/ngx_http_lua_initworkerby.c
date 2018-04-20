openresty/lua-nginx-module

commit 012f3163eb67bad2f664b459285913ea5afd6449

optimize: destroy the Lua VM and avoid running any init_worker_by_lua* code inside cache helper processes.
Signed-off-by: Yichun Zhang (agentzh) <agentzh@gmail.com>

src/ngx_http_lua_initworkerby.c
@@ -40,10 +40,30 @@ ngx_http_lua_init_worker(ngx_cycle_t *cycle)
 
     lmcf = ngx_http_cycle_get_module_main_conf(cycle, ngx_http_lua_module);
 
-    if (lmcf == NULL
-        || lmcf->init_worker_handler == NULL
-        || lmcf->lua == NULL)
+    if (lmcf == NULL || lmcf->lua == NULL) {
+        return NGX_OK;
+    }
+
+    /* lmcf != NULL && lmcf->lua != NULL */
+
+    /* disable init_worker_by_lua* and destroy lua VM in cache processes */
+    if (ngx_process == NGX_PROCESS_HELPER
+#if defined(HAVE_PRIVILEGED_PROCESS_PATCH) && !NGX_WIN32
+        && !ngx_is_privileged_agent
+#endif
+       )
     {
+        ngx_log_debug2(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
+                       "lua close the global Lua VM %p in the "
+                       "cache helper process %P", lmcf->lua, ngx_pid);
+
+        lmcf->vm_cleanup->handler(lmcf->vm_cleanup->data);
+        lmcf->vm_cleanup->handler = NULL;
+
+        return NGX_OK;
+    }
+
+    if (lmcf->init_worker_handler == NULL) {
         return NGX_OK;
     }
 

README.markdown
@@ -1487,6 +1487,8 @@ This hook is often used to create per-worker reoccurring timers (via the [ngx.ti
 
 This directive was first introduced in the `v0.9.5` release.
 
+This hook no longer runs in the cache manager and cache loader processes since the `v0.10.12` release.
+
 [Back to TOC](#directives)
 
 init_worker_by_lua_block
@@ -1514,6 +1516,8 @@ For instance,
 
 This directive was first introduced in the `v0.9.17` release.
 
+This hook no longer runs in the cache manager and cache loader processes since the `v0.10.12` release.
+
 [Back to TOC](#directives)
 
 init_worker_by_lua_file
@@ -1528,6 +1532,8 @@ init_worker_by_lua_file
 Similar to [init_worker_by_lua](#init_worker_by_lua), but accepts the file path to a Lua source file or Lua bytecode file.
 
 This directive was first introduced in the `v0.9.5` release.
+
+This hook no longer runs in the cache manager and cache loader processes since the `v0.10.12` release.
 
 [Back to TOC](#directives)
 

doc/HttpLuaModule.wiki
@@ -1196,6 +1196,8 @@ This hook is often used to create per-worker reoccurring timers (via the [[#ngx.
 
 This directive was first introduced in the <code>v0.9.5</code> release.
 
+This hook no longer runs in the cache manager and cache loader processes since the <code>v0.10.12</code> release.
+
 == init_worker_by_lua_block ==
 
 '''syntax:''' ''init_worker_by_lua_block { lua-script }''
@@ -1219,6 +1221,8 @@ For instance,
 
 This directive was first introduced in the <code>v0.9.17</code> release.
 
+This hook no longer runs in the cache manager and cache loader processes since the <code>v0.10.12</code> release.
+
 == init_worker_by_lua_file ==
 
 '''syntax:''' ''init_worker_by_lua_file <lua-file-path>''
@@ -1230,6 +1234,8 @@ This directive was first introduced in the <code>v0.9.17</code> release.
 Similar to [[#init_worker_by_lua|init_worker_by_lua]], but accepts the file path to a Lua source file or Lua bytecode file.
 
 This directive was first introduced in the <code>v0.9.5</code> release.
+
+This hook no longer runs in the cache manager and cache loader processes since the <code>v0.10.12</code> release.
 
 == set_by_lua ==
 

src/ngx_http_lua_common.h
@@ -162,6 +162,7 @@ typedef struct {
 
 struct ngx_http_lua_main_conf_s {
     lua_State           *lua;
+    ngx_pool_cleanup_t  *vm_cleanup;
 
     ngx_str_t            lua_path;
     ngx_str_t            lua_cpath;

src/ngx_http_lua_util.c
@@ -3768,6 +3768,12 @@ ngx_http_lua_init_vm(lua_State *parent_vm, ngx_cycle_t *cycle,
 
     cln->data = state;
 
+    if (lmcf->vm_cleanup == NULL) {
+        /* this assignment will happen only once,
+         * and also only for the main Lua VM */
+        lmcf->vm_cleanup = cln;
+    }
+
     if (pcln) {
         *pcln = cln;
     }

t/124-init-worker.t
@@ -3,13 +3,13 @@
 use Test::Nginx::Socket::Lua;
 
 #worker_connections(1014);
-#master_on();
+master_on();
 #workers(2);
 #log_level('warn');
 
 repeat_each(1);
 
-plan tests => repeat_each() * (blocks() * 4);
+plan tests => repeat_each() * (blocks() * 4 + 4);
 
 $ENV{TEST_NGINX_MEMCACHED_PORT} ||= 11211;
 $ENV{TEST_NGINX_RESOLVER} ||= '8.8.8.8';
@@ -756,3 +756,183 @@ GET /t
 ok
 --- no_error_log
 [error]
+
+
+
+=== TEST 20: destory Lua VM in cache processes (without privileged agent or shdict)
+--- http_config
+    lua_package_path "../lua-resty-core/lib/?.lua;../lua-resty-lrucache/lib/?.lua;;";
+
+    proxy_cache_path /tmp/cache levels=1:2 keys_zone=cache:1m;
+
+    #lua_shared_dict dummy 500k;
+
+    init_by_lua_block {
+        -- require "resty.core.regex"
+        -- assert(ngx.re.match("hello, world", [[hello, \w+]], "joi"))
+    }
+
+--- config
+    location = /t {
+        return 200;
+    }
+--- request
+    GET /t
+--- grep_error_log eval: qr/lua close the global Lua VM \S+ in the cache helper process \d+|lua close the global Lua VM \S+$/
+--- grep_error_log_out eval
+qr/\Alua close the global Lua VM ([0-9A-F]+) in the cache helper process \d+
+lua close the global Lua VM \1
+lua close the global Lua VM \1 in the cache helper process \d+
+lua close the global Lua VM \1
+(?:lua close the global Lua VM [0-9A-F]+
+)*\z/
+--- no_error_log
+[error]
+start privileged agent process
+
+
+
+=== TEST 21: destory Lua VM in cache processes (without privileged agent but with shdict)
+--- http_config
+    lua_package_path "../lua-resty-core/lib/?.lua;../lua-resty-lrucache/lib/?.lua;;";
+
+    proxy_cache_path /tmp/cache levels=1:2 keys_zone=cache:1m;
+
+    lua_shared_dict dummy 500k;
+
+    init_by_lua_block {
+        -- require "resty.core.regex"
+        -- assert(ngx.re.match("hello, world", [[hello, \w+]], "joi"))
+    }
+
+--- config
+    location = /t {
+        return 200;
+    }
+--- request
+    GET /t
+--- grep_error_log eval: qr/lua close the global Lua VM \S+ in the cache helper process \d+|lua close the global Lua VM \S+$/
+--- grep_error_log_out eval
+qr/\Alua close the global Lua VM ([0-9A-F]+) in the cache helper process \d+
+lua close the global Lua VM \1
+lua close the global Lua VM \1 in the cache helper process \d+
+lua close the global Lua VM \1
+(?:lua close the global Lua VM [0-9A-F]+
+)*\z/
+--- no_error_log
+[error]
+start privileged agent process
+
+
+
+=== TEST 22: destory Lua VM in cache processes (with privileged agent)
+--- http_config
+    lua_package_path "../lua-resty-core/lib/?.lua;../lua-resty-lrucache/lib/?.lua;;";
+
+    #lua_shared_dict dogs 1m;
+
+    proxy_cache_path /tmp/cache levels=1:2 keys_zone=cache:1m;
+
+    init_by_lua_block {
+        assert(require "ngx.process".enable_privileged_agent())
+        -- require "resty.core.regex"
+        -- assert(ngx.re.match("hello, world", [[hello, \w+]], "joi"))
+    }
+
+--- config
+    location = /t {
+        return 200;
+    }
+--- request
+    GET /t
+--- grep_error_log eval: qr/lua close the global Lua VM \S+ in the cache helper process \d+|lua close the global Lua VM \S+$/
+--- grep_error_log_out eval
+qr/\Alua close the global Lua VM ([0-9A-F]+) in the cache helper process \d+
+lua close the global Lua VM \1
+lua close the global Lua VM \1 in the cache helper process \d+
+lua close the global Lua VM \1
+(?:lua close the global Lua VM [0-9A-F]+
+)*\z/
+--- error_log eval
+qr/start privileged agent process \d+/
+--- no_error_log
+[error]
+
+
+
+=== TEST 23: destory Lua VM in cache processes (with init worker and privileged agent)
+--- http_config
+    lua_package_path "../lua-resty-core/lib/?.lua;../lua-resty-lrucache/lib/?.lua;;";
+
+    #lua_shared_dict dogs 1m;
+
+    proxy_cache_path /tmp/cache levels=1:2 keys_zone=cache:1m;
+
+    init_by_lua_block {
+        assert(require "ngx.process".enable_privileged_agent())
+        -- require "resty.core.regex"
+        -- assert(ngx.re.match("hello, world", [[hello, \w+]], "joi"))
+    }
+
+    init_worker_by_lua_block {
+        ngx.log(ngx.WARN, "hello from init worker by lua")
+    }
+
+--- config
+    location = /t {
+        return 200;
+    }
+--- request
+    GET /t
+--- grep_error_log eval: qr/hello from init worker by lua/
+--- grep_error_log_out
+hello from init worker by lua
+hello from init worker by lua
+
+--- error_log eval
+[
+qr/start privileged agent process \d+$/,
+qr/lua close the global Lua VM ([0-9A-F]+) in the cache helper process \d+$/,
+qr/lua close the global Lua VM ([0-9A-F]+)$/,
+]
+--- no_error_log
+[error]
+
+
+
+=== TEST 24: destory Lua VM in cache processes (with init worker but without privileged agent)
+--- http_config
+    lua_package_path "../lua-resty-core/lib/?.lua;../lua-resty-lrucache/lib/?.lua;;";
+
+    #lua_shared_dict dogs 1m;
+
+    proxy_cache_path /tmp/cache levels=1:2 keys_zone=cache:1m;
+
+    init_by_lua_block {
+        -- require "resty.core.regex"
+        -- assert(ngx.re.match("hello, world", [[hello, \w+]], "joi"))
+    }
+
+    init_worker_by_lua_block {
+        ngx.log(ngx.WARN, "hello from init worker by lua")
+    }
+
+--- config
+    location = /t {
+        return 200;
+    }
+--- request
+    GET /t
+
+--- grep_error_log eval: qr/hello from init worker by lua/
+--- grep_error_log_out
+hello from init worker by lua
+
+--- error_log eval
+[
+qr/lua close the global Lua VM ([0-9A-F]+) in the cache helper process \d+$/,
+qr/lua close the global Lua VM ([0-9A-F]+)$/,
+]
+--- no_error_log
+[error]
+start privileged agent process

t/135-worker-id.t
@@ -9,7 +9,7 @@ workers(2);
 
 #repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 3 + 1);
+plan tests => repeat_each() * (blocks() * 3);
 
 #no_diff();
 #no_long_string();
@@ -31,63 +31,3 @@ GET /lua
 --- no_error_log
 [error]
 --- skip_nginx: 3: <=1.9.0
-
-
-
-=== TEST 2: worker id should be nil for non-worker processes
---- http_config
-    proxy_cache_path conf/cache levels=1:2 keys_zone=my-cache:8m max_size=10m inactive=60m;
-    proxy_temp_path conf/temp;
-
-    lua_shared_dict counters 1m;
-
-    init_by_lua_block {
-        ngx.shared.counters:set("c", 0)
-    }
-
-    init_worker_by_lua_block {
-        ngx.shared.counters:incr("c", 1)
-        ngx.log(ngx.INFO, ngx.worker.pid(), ": worker id ", ngx.worker.id());
-    }
---- config
-    location = /t {
-        content_by_lua_block {
-            local counters = ngx.shared.counters
-            local ok, c
-            for i = 1, 100 do
-                c = counters:get("c")
-                if c >= 4 then
-                    ok = true
-                    break
-                end
-                local delay = 0.001 * i
-                if delay > 0.1 then
-                    delay = 0.1
-                end
-                ngx.sleep(delay)
-            end
-            if ok then
-                ngx.say("ok")
-            else
-                ngx.say("not ok: c=", c)
-            end
-        }
-    }
-    location /cache {
-        proxy_pass http://127.0.0.1:$server_port;
-        proxy_cache my-cache;
-    }
---- request
-GET /t
---- response_body
-ok
---- grep_error_log eval: qr/worker id nil/
---- grep_error_log_out
-worker id nil
-worker id nil
---- no_error_log
-[error]
---- wait: 0.1
---- skip_nginx: 3: <=1.9.0
---- log_level: info
---- timeout: 6

commit 78e447e90d8474b01cc6a70b1c76e58202713d8f

bugfix: fixed compatibility issues with MinGW gcc. this regression had appeared in commit 012f3163e.

src/ngx_http_lua_initworkerby.c
@@ -47,12 +47,8 @@ ngx_http_lua_init_worker(ngx_cycle_t *cycle)
     /* lmcf != NULL && lmcf->lua != NULL */
 
     /* disable init_worker_by_lua* and destroy lua VM in cache processes */
-    if (ngx_process == NGX_PROCESS_HELPER
 #if defined(HAVE_PRIVILEGED_PROCESS_PATCH) && !NGX_WIN32
-        && !ngx_is_privileged_agent
-#endif
-       )
-    {
+    if (ngx_process == NGX_PROCESS_HELPER && !ngx_is_privileged_agent) {
         ngx_log_debug2(NGX_LOG_DEBUG_HTTP, ngx_cycle->log, 0,
                        "lua close the global Lua VM %p in the "
                        "cache helper process %P", lmcf->lua, ngx_pid);
@@ -62,6 +58,7 @@ ngx_http_lua_init_worker(ngx_cycle_t *cycle)
 
         return NGX_OK;
     }
+#endif
 
     if (lmcf->init_worker_handler == NULL) {
         return NGX_OK;