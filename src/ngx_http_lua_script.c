d067594ca299ec00fcbfe98288844d3bf10e8064
Alessandro Ghedini <alessandro@cloudflare.com>
2018-08-31 14:42:55 +0100

    bugfix: silenced -Wcast-function-type warnings.
    Cast to intermediate "void *" to lose compiler knowledge about the
    original type and pass a gcc8 warning.
    
    Signed-off-by: Thibault Charbonnier <thibaultcha@me.com>

--- src/ngx_http_lua_script.c
+++ src/ngx_http_lua_script.c
@@ -329,7 +329,7 @@ ngx_http_lua_script_add_copy_code(ngx_http_lua_script_compile_t *sc,
         return NGX_ERROR;
     }
 
-    code->code = (ngx_http_lua_script_code_pt)
+    code->code = (ngx_http_lua_script_code_pt) (void *)
                  ngx_http_lua_script_copy_len_code;
     code->len = len;
 
@@ -399,7 +399,7 @@ ngx_http_lua_script_add_capture_code(ngx_http_lua_script_compile_t *sc,
         return NGX_ERROR;
     }
 
-    code->code = (ngx_http_lua_script_code_pt)
+    code->code = (ngx_http_lua_script_code_pt) (void *)
                  ngx_http_lua_script_copy_capture_len_code;
     code->n = 2 * n;
 
