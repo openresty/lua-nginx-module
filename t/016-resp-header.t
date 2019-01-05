openresty/lua-nginx-module

commit 017e00462ae1bea96f508365726f7cec3cbcc5ca

bugfix: we now avoid generating a Content-Type header when getting/setting other response headers.
Signed-off-by: Thibault Charbonnier <thibaultcha@me.com>

src/ngx_http_lua_headers.c
@@ -544,17 +544,6 @@ ngx_http_lua_ngx_resp_get_headers(lua_State *L)
         return luaL_error(L, "no ctx found");
     }
 
-    if (!ctx->headers_set) {
-        rc = ngx_http_lua_set_content_type(r);
-        if (rc != NGX_OK) {
-            return luaL_error(L,
-                              "failed to set default content type: %d",
-                              (int) rc);
-        }
-
-        ctx->headers_set = 1;
-    }
-
     ngx_http_lua_check_fake_request(L, r);
 
     part = &r->headers_out.headers.part;
@@ -571,6 +560,17 @@ ngx_http_lua_ngx_resp_get_headers(lua_State *L)
                               headers_metatable_key));
         lua_rawget(L, LUA_REGISTRYINDEX);
         lua_setmetatable(L, -2);
+    }
+
+    if (!r->headers_out.content_type.len) {
+        rc = ngx_http_lua_set_content_type(r);
+        if (rc != NGX_OK) {
+            return luaL_error(L,
+                              "failed to set default content type: %d",
+                              (int) rc);
+        }
+
+        ctx->headers_set = 1;
     }
 
 #if 1
@@ -696,7 +696,6 @@ ngx_http_lua_ngx_header_get(lua_State *L)
     size_t                       len;
     ngx_http_lua_loc_conf_t     *llcf;
     ngx_http_lua_ctx_t          *ctx;
-    ngx_int_t                    rc;
 
     r = ngx_http_lua_get_req(L);
     if (r == NULL) {
@@ -739,18 +738,7 @@ ngx_http_lua_ngx_header_get(lua_State *L)
 
     key.len = len;
 
-    if (!ctx->headers_set) {
-        rc = ngx_http_lua_set_content_type(r);
-        if (rc != NGX_OK) {
-            return luaL_error(L,
-                              "failed to set default content type: %d",
-                              (int) rc);
-        }
-
-        ctx->headers_set = 1;
-    }
-
-    return ngx_http_lua_get_output_header(L, r, &key);
+    return ngx_http_lua_get_output_header(L, r, ctx, &key);
 }
 
 
@@ -813,16 +801,7 @@ ngx_http_lua_ngx_header_set(lua_State *L)
         }
     }
 
-    if (!ctx->headers_set) {
-        rc = ngx_http_lua_set_content_type(r);
-        if (rc != NGX_OK) {
-            return luaL_error(L,
-                              "failed to set default content type: %d",
-                              (int) rc);
-        }
-
-        ctx->headers_set = 1;
-    }
+    ctx->headers_set = 1;
 
     if (lua_type(L, 3) == LUA_TNIL) {
         ngx_str_null(&value);
@@ -1244,15 +1223,7 @@ ngx_http_lua_ffi_set_resp_header(ngx_http_request_t *r, const u_char *key_data,
         }
     }
 
-    if (!ctx->headers_set) {
-        rc = ngx_http_lua_set_content_type(r);
-        if (rc != NGX_OK) {
-            *errmsg = "failed to set default content type";
-            return NGX_ERROR;
-        }
-
-        ctx->headers_set = 1;
-    }
+    ctx->headers_set = 1;
 
     if (is_nil) {
         value.data = NULL;
@@ -1373,7 +1344,8 @@ ngx_http_lua_ffi_req_header_set_single_value(ngx_http_request_t *r,
 int
 ngx_http_lua_ffi_get_resp_header(ngx_http_request_t *r,
     const u_char *key, size_t key_len,
-    u_char *key_buf, ngx_http_lua_ffi_str_t *values, int max_nvalues)
+    u_char *key_buf, ngx_http_lua_ffi_str_t *values, int max_nvalues,
+    char **errmsg)
 {
     int                  found;
     u_char               c, *p;
@@ -1390,17 +1362,8 @@ ngx_http_lua_ffi_get_resp_header(ngx_http_request_t *r,
 
     ctx = ngx_http_get_module_ctx(r, ngx_http_lua_module);
     if (ctx == NULL) {
-        /* *errmsg = "no ctx found"; */
+        *errmsg = "no ctx found";
         return NGX_ERROR;
-    }
-
-    if (!ctx->headers_set) {
-        if (ngx_http_lua_set_content_type(r) != NGX_OK) {
-            /* *errmsg = "failed to set default content type"; */
-            return NGX_ERROR;
-        }
-
-        ctx->headers_set = 1;
     }
 
     llcf = ngx_http_get_module_loc_conf(r, ngx_http_lua_module);
@@ -1428,6 +1391,7 @@ ngx_http_lua_ffi_get_resp_header(ngx_http_request_t *r,
         {
             p = ngx_palloc(r->pool, NGX_OFF_T_LEN);
             if (p == NULL) {
+                *errmsg = "no memory";
                 return NGX_ERROR;
             }
 
@@ -1441,12 +1405,21 @@ ngx_http_lua_ffi_get_resp_header(ngx_http_request_t *r,
         break;
 
     case 12:
-        if (r->headers_out.content_type.len
-            && ngx_strncasecmp(key_buf, (u_char *) "Content-Type", 12) == 0)
-        {
-            values[0].data = r->headers_out.content_type.data;
-            values[0].len = r->headers_out.content_type.len;
-            return 1;
+        if (ngx_strncasecmp(key_buf, (u_char *) "Content-Type", 12) == 0) {
+            if (!r->headers_out.content_type.len) {
+                if (ngx_http_lua_set_content_type(r) != NGX_OK) {
+                    *errmsg = "failed to set default content type";
+                    return NGX_ERROR;
+                }
+
+                ctx->headers_set = 1;
+            }
+
+            if (r->headers_out.content_type.len) {
+                values[0].data = r->headers_out.content_type.data;
+                values[0].len = r->headers_out.content_type.len;
+                return 1;
+            }
         }
 
         break;

src/ngx_http_lua_headers_out.c
@@ -528,10 +528,11 @@ ngx_http_lua_set_output_header(ngx_http_request_t *r, ngx_str_t key,
 
 int
 ngx_http_lua_get_output_header(lua_State *L, ngx_http_request_t *r,
-    ngx_str_t *key)
+    ngx_http_lua_ctx_t *ctx, ngx_str_t *key)
 {
     ngx_table_elt_t            *h;
     ngx_list_part_t            *part;
+    ngx_int_t                   rc;
     ngx_uint_t                  i;
     unsigned                    found;
 
@@ -550,12 +551,23 @@ ngx_http_lua_get_output_header(lua_State *L, ngx_http_request_t *r,
         break;
 
     case 12:
-        if (r->headers_out.content_type.len
-            && ngx_strncasecmp(key->data, (u_char *) "Content-Type", 12) == 0)
-        {
-            lua_pushlstring(L, (char *) r->headers_out.content_type.data,
-                            r->headers_out.content_type.len);
-            return 1;
+        if (ngx_strncasecmp(key->data, (u_char *) "Content-Type", 12) == 0) {
+            if (!r->headers_out.content_type.len) {
+                rc = ngx_http_lua_set_content_type(r);
+                if (rc != NGX_OK) {
+                    return luaL_error(L,
+                                      "failed to set default content type: %d",
+                                      (int) rc);
+                }
+
+                ctx->headers_set = 1;
+            }
+
+            if (r->headers_out.content_type.len) {
+                lua_pushlstring(L, (char *) r->headers_out.content_type.data,
+                                r->headers_out.content_type.len);
+                return 1;
+            }
         }
 
         break;

src/ngx_http_lua_headers_out.h
@@ -15,7 +15,7 @@
 ngx_int_t ngx_http_lua_set_output_header(ngx_http_request_t *r, ngx_str_t key,
     ngx_str_t value, unsigned override);
 int ngx_http_lua_get_output_header(lua_State *L, ngx_http_request_t *r,
-    ngx_str_t *key);
+    ngx_http_lua_ctx_t *ctx, ngx_str_t *key);
 
 
 #endif /* _NGX_HTTP_LUA_HEADERS_OUT_H_INCLUDED_ */

src/ngx_http_lua_subrequest.c
@@ -1031,6 +1031,14 @@ ngx_http_lua_post_subrequest(ngx_http_request_t *r, void *data, ngx_int_t rc)
     dd("pr_coctx status: %d", (int) pr_coctx->sr_statuses[ctx->index]);
 
     /* copy subrequest response headers */
+    if (ctx->headers_set) {
+        rc = ngx_http_lua_set_content_type(r);
+        if (rc != NGX_OK) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                          "failed to set default content type: %i", rc);
+            return NGX_ERROR;
+        }
+    }
 
     pr_coctx->sr_headers[ctx->index] = &r->headers_out;
 

t/016-resp-header.t
@@ -8,7 +8,7 @@ use Test::Nginx::Socket::Lua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 3 + 46);
+plan tests => repeat_each() * (blocks() * 3 + 51);
 
 #no_diff();
 no_long_string();
@@ -781,7 +781,10 @@ GET /read
 --- more_headers
 Accept-Encoding: gzip
 --- response_headers
-Content-Type: text/plain
+Content-Encoding: gzip
+--- no_error_log
+[error]
+http gzip filter
 --- response_body
 Hello, world, my dear friend!
 
@@ -1699,3 +1702,62 @@ found 3 resp headers
 --- no_error_log
 [error]
 lua exceeding response header limit
+
+
+
+=== TEST 76: don't generate Content-Type header when setting other response headers
+--- config
+    location = /t {
+        default_type text/html;
+        rewrite_by_lua_block {
+            ngx.header.blah = "foo"
+        }
+        proxy_pass http://127.0.0.1:$TEST_NGINX_SERVER_PORT/backend;
+    }
+
+    location = /backend {
+        content_by_lua_block {
+            ngx.say("foo")
+        }
+        header_filter_by_lua_block {
+            ngx.header.content_type = nil
+        }
+    }
+--- request
+GET /t
+--- response_body
+foo
+--- response_headers
+blah: foo
+Content-Type:
+--- no_error_log
+[error]
+
+
+
+=== TEST 77: don't generate Content-Type header when getting other response headers
+--- config
+    location = /t {
+        default_type text/html;
+        rewrite_by_lua_block {
+            local h = ngx.header.content_length
+        }
+        proxy_pass http://127.0.0.1:$TEST_NGINX_SERVER_PORT/backend;
+    }
+
+    location = /backend {
+        content_by_lua_block {
+            ngx.say("foo")
+        }
+        header_filter_by_lua_block {
+            ngx.header.content_type = nil
+        }
+    }
+--- request
+GET /t
+--- response_body
+foo
+--- response_headers
+Content-Type:
+--- no_error_log
+[error]

README.markdown
@@ -4147,6 +4147,25 @@ to be returned when reading `ngx.header.Foo`.
 
 Note that `ngx.header` is not a normal Lua table and as such, it is not possible to iterate through it using the Lua `ipairs` function.
 
+Note: if the current response does not have a Content-Type header, but you use
+`ngx.header` to attempt retrieving it, a side effect will be that a
+Content-Type header will be created and returned according to your nginx
+configuration.
+
+For example:
+
+```nginx
+
+ location = /t {
+     default_type text/html;
+     content_by_lua_block {
+         -- even if the response had no Content-Type header,
+         -- it will now have: Content-Type: text/html
+         ngx.say(ngx.header['Content-Type'])
+     }
+ }
+```
+
 For reading *request* headers, use the [ngx.req.get_headers](#ngxreqget_headers) function instead.
 
 [Back to TOC](#nginx-api-for-lua)
@@ -4170,6 +4189,24 @@ Returns a Lua table holding all the current response headers for the current req
  for k, v in pairs(h) do
      ...
  end
+```
+
+Note: if the current response does not have a Content-Type header, but you use
+`ngx.resp.get_headers` to attempt retrieving it, a side effect will
+be that a Content-Type header will be created and returned according to your
+nginx configuration.
+
+For example:
+
+```nginx
+
+ location = /t {
+     default_type text/html;
+     content_by_lua_block {
+         -- the Content-Type header will be text/html
+         ngx.say(ngx.resp.get_headers()['Content-Type'])
+     }
+ }
 ```
 
 This function has the same signature as [ngx.req.get_headers](#ngxreqget_headers) except getting response headers instead of request headers.

doc/HttpLuaModule.wiki
@@ -3433,6 +3433,24 @@ to be returned when reading <code>ngx.header.Foo</code>.
 
 Note that <code>ngx.header</code> is not a normal Lua table and as such, it is not possible to iterate through it using the Lua <code>ipairs</code> function.
 
+Note: if the current response does not have a Content-Type header, but you use
+<code>ngx.header</code> to attempt retrieving it, a side effect will be that a
+Content-Type header will be created and returned according to your nginx
+configuration.
+
+For example:
+
+<geshi lang="nginx">
+    location = /t {
+        default_type text/html;
+        content_by_lua_block {
+            -- even if the response had no Content-Type header,
+            -- it will now have: Content-Type: text/html
+            ngx.say(ngx.header['Content-Type'])
+        }
+    }
+</geshi>
+
 For reading ''request'' headers, use the [[#ngx.req.get_headers|ngx.req.get_headers]] function instead.
 
 == ngx.resp.get_headers ==
@@ -3452,6 +3470,23 @@ end
 for k, v in pairs(h) do
     ...
 end
+</geshi>
+
+Note: if the current response does not have a Content-Type header, but you use
+<code>ngx.resp.get_headers</code>, a side effect will be that a Content-Type
+header will be created and returned according to your nginx configuration.
+
+For example:
+
+<geshi lang="nginx">
+    location = /t {
+        default_type text/html;
+        content_by_lua_block {
+            -- even if the response had no Content-Type header,
+            -- it will now have: Content-Type: text/html
+            ngx.say(ngx.resp.get_headers()['Content-Type'])
+        }
+    }
 </geshi>
 
 This function has the same signature as [[#ngx.req.get_headers|ngx.req.get_headers]] except getting response headers instead of request headers.

commit d2e92590ad75f2b8f19f07ebd1642d4bc863e6fe

bugfix: ensured Content-Type header gets generated when other headers get set.
Fixed a regression introduced in 017e004 and previously described
in https://github.com/openresty/lua-nginx-module/issues/92.

Signed-off-by: Thibault Charbonnier <thibaultcha@me.com>

src/ngx_http_lua_common.h
@@ -566,6 +566,8 @@ typedef struct ngx_http_lua_ctx_s {
 
     unsigned         headers_set:1; /* whether the user has set custom
                                        response headers */
+    unsigned         mime_set:1;    /* whether the user has set Content-Type
+                                       response header */
 
     unsigned         entered_rewrite_phase:1;
     unsigned         entered_access_phase:1;

src/ngx_http_lua_headers.c
@@ -563,7 +563,7 @@ ngx_http_lua_ngx_resp_get_headers(lua_State *L)
     }
 
     if (!r->headers_out.content_type.len) {
-        rc = ngx_http_lua_set_content_type(r);
+        rc = ngx_http_lua_set_content_type(r, ctx);
         if (rc != NGX_OK) {
             return luaL_error(L,
                               "failed to set default content type: %d",
@@ -826,7 +826,7 @@ ngx_http_lua_ngx_header_set(lua_State *L)
                 ngx_memcpy(value.data, p, len);
                 value.len = len;
 
-                rc = ngx_http_lua_set_output_header(r, key, value,
+                rc = ngx_http_lua_set_output_header(r, ctx, key, value,
                                                     i == 1 /* override */);
 
                 if (rc == NGX_ERROR) {
@@ -853,7 +853,7 @@ ngx_http_lua_ngx_header_set(lua_State *L)
     dd("key: %.*s, value: %.*s",
        (int) key.len, key.data, (int) value.len, value.data);
 
-    rc = ngx_http_lua_set_output_header(r, key, value, 1 /* override */);
+    rc = ngx_http_lua_set_output_header(r, ctx, key, value, 1 /* override */);
 
     if (rc == NGX_ERROR) {
         return luaL_error(L, "failed to set header %s (error: %d)",
@@ -1250,7 +1250,7 @@ ngx_http_lua_ffi_set_resp_header(ngx_http_request_t *r, const u_char *key_data,
                 ngx_memcpy(value.data, p, len);
                 value.len = len;
 
-                rc = ngx_http_lua_set_output_header(r, key, value,
+                rc = ngx_http_lua_set_output_header(r, ctx, key, value,
                                                     override && i == 0);
 
                 if (rc == NGX_ERROR) {
@@ -1276,7 +1276,7 @@ ngx_http_lua_ffi_set_resp_header(ngx_http_request_t *r, const u_char *key_data,
     dd("key: %.*s, value: %.*s",
        (int) key.len, key.data, (int) value.len, value.data);
 
-    rc = ngx_http_lua_set_output_header(r, key, value, override);
+    rc = ngx_http_lua_set_output_header(r, ctx, key, value, override);
 
     if (rc == NGX_ERROR) {
         *errmsg = "failed to set header";
@@ -1407,7 +1407,7 @@ ngx_http_lua_ffi_get_resp_header(ngx_http_request_t *r,
     case 12:
         if (ngx_strncasecmp(key_buf, (u_char *) "Content-Type", 12) == 0) {
             if (!r->headers_out.content_type.len) {
-                if (ngx_http_lua_set_content_type(r) != NGX_OK) {
+                if (ngx_http_lua_set_content_type(r, ctx) != NGX_OK) {
                     *errmsg = "failed to set default content type";
                     return NGX_ERROR;
                 }

src/ngx_http_lua_headers_out.c
@@ -476,8 +476,8 @@ ngx_http_clear_builtin_header(ngx_http_request_t *r,
 
 
 ngx_int_t
-ngx_http_lua_set_output_header(ngx_http_request_t *r, ngx_str_t key,
-    ngx_str_t value, unsigned override)
+ngx_http_lua_set_output_header(ngx_http_request_t *r, ngx_http_lua_ctx_t *ctx,
+    ngx_str_t key, ngx_str_t value, unsigned override)
 {
     ngx_http_lua_header_val_t         hv;
     ngx_http_lua_set_header_t        *handlers = ngx_http_lua_set_handlers;
@@ -507,6 +507,10 @@ ngx_http_lua_set_output_header(ngx_http_request_t *r, ngx_str_t key,
 
         hv.offset = handlers[i].offset;
         hv.handler = handlers[i].handler;
+
+        if (hv.handler == ngx_http_set_content_type_header) {
+            ctx->mime_set = 1;
+        }
 
         break;
     }
@@ -552,8 +556,8 @@ ngx_http_lua_get_output_header(lua_State *L, ngx_http_request_t *r,
 
     case 12:
         if (ngx_strncasecmp(key->data, (u_char *) "Content-Type", 12) == 0) {
-            if (!r->headers_out.content_type.len) {
-                rc = ngx_http_lua_set_content_type(r);
+            if (r->headers_out.content_type.len == 0) {
+                rc = ngx_http_lua_set_content_type(r, ctx);
                 if (rc != NGX_OK) {
                     return luaL_error(L,
                                       "failed to set default content type: %d",

src/ngx_http_lua_headers_out.h
@@ -12,8 +12,8 @@
 #include "ngx_http_lua_common.h"
 
 
-ngx_int_t ngx_http_lua_set_output_header(ngx_http_request_t *r, ngx_str_t key,
-    ngx_str_t value, unsigned override);
+ngx_int_t ngx_http_lua_set_output_header(ngx_http_request_t *r,
+    ngx_http_lua_ctx_t *ctx, ngx_str_t key, ngx_str_t value, unsigned override);
 int ngx_http_lua_get_output_header(lua_State *L, ngx_http_request_t *r,
     ngx_http_lua_ctx_t *ctx, ngx_str_t *key);
 

src/ngx_http_lua_subrequest.c
@@ -1032,7 +1032,7 @@ ngx_http_lua_post_subrequest(ngx_http_request_t *r, void *data, ngx_int_t rc)
 
     /* copy subrequest response headers */
     if (ctx->headers_set) {
-        rc = ngx_http_lua_set_content_type(r);
+        rc = ngx_http_lua_set_content_type(r, ctx);
         if (rc != NGX_OK) {
             ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
                           "failed to set default content type: %i", rc);

src/ngx_http_lua_util.c
@@ -433,7 +433,9 @@ ngx_http_lua_send_header_if_needed(ngx_http_request_t *r,
             r->headers_out.status = NGX_HTTP_OK;
         }
 
-        if (!ctx->headers_set && ngx_http_lua_set_content_type(r) != NGX_OK) {
+        if (!ctx->mime_set
+            && ngx_http_lua_set_content_type(r, ctx) != NGX_OK)
+        {
             return NGX_ERROR;
         }
 

src/ngx_http_lua_util.h
@@ -411,9 +411,11 @@ ngx_http_lua_hash_str(u_char *src, size_t n)
 
 
 static ngx_inline ngx_int_t
-ngx_http_lua_set_content_type(ngx_http_request_t *r)
+ngx_http_lua_set_content_type(ngx_http_request_t *r, ngx_http_lua_ctx_t *ctx)
 {
     ngx_http_lua_loc_conf_t     *llcf;
+
+    ctx->mime_set = 1;
 
     llcf = ngx_http_get_module_loc_conf(r, ngx_http_lua_module);
     if (llcf->use_default_type

t/016-resp-header.t
@@ -8,7 +8,7 @@ use Test::Nginx::Socket::Lua;
 
 repeat_each(2);
 
-plan tests => repeat_each() * (blocks() * 3 + 51);
+plan tests => repeat_each() * (blocks() * 3 + 55);
 
 #no_diff();
 no_long_string();
@@ -1705,8 +1705,17 @@ lua exceeding response header limit
 
 
 
-=== TEST 76: don't generate Content-Type header when setting other response headers
+=== TEST 76: don't generate Content-Type when setting other response header
 --- config
+    location = /backend {
+        content_by_lua_block {
+            ngx.say("foo")
+        }
+        header_filter_by_lua_block {
+            ngx.header.content_type = nil
+        }
+    }
+
     location = /t {
         default_type text/html;
         rewrite_by_lua_block {
@@ -1714,13 +1723,54 @@ lua exceeding response header limit
         }
         proxy_pass http://127.0.0.1:$TEST_NGINX_SERVER_PORT/backend;
     }
+--- request
+GET /t
+--- response_body
+foo
+--- response_headers
+blah: foo
+!Content-Type
+--- no_error_log
+[error]
 
+
+
+=== TEST 77: don't generate Content-Type when getting other response header
+--- config
     location = /backend {
         content_by_lua_block {
             ngx.say("foo")
         }
         header_filter_by_lua_block {
             ngx.header.content_type = nil
+        }
+    }
+
+    location = /t {
+        default_type text/html;
+        rewrite_by_lua_block {
+            local h = ngx.header.content_length
+        }
+        proxy_pass http://127.0.0.1:$TEST_NGINX_SERVER_PORT/backend;
+    }
+--- request
+GET /t
+--- response_body
+foo
+--- response_headers
+!Content-Type
+--- no_error_log
+[error]
+
+
+
+=== TEST 78: generate default Content-Type when setting other response header
+--- config
+    location = /t {
+        default_type text/html;
+        content_by_lua_block {
+            ngx.header.blah = "foo"
+            ngx.say("foo")
         }
     }
 --- request
@@ -1729,28 +1779,19 @@ GET /t
 foo
 --- response_headers
 blah: foo
-Content-Type:
+Content-Type: text/html
 --- no_error_log
 [error]
 
 
 
-=== TEST 77: don't generate Content-Type header when getting other response headers
+=== TEST 79: don't generate default Content-Type when Content-Type is cleared
 --- config
     location = /t {
         default_type text/html;
-        rewrite_by_lua_block {
-            local h = ngx.header.content_length
-        }
-        proxy_pass http://127.0.0.1:$TEST_NGINX_SERVER_PORT/backend;
-    }
-
-    location = /backend {
         content_by_lua_block {
+            ngx.header["Content-Type"] = nil
             ngx.say("foo")
-        }
-        header_filter_by_lua_block {
-            ngx.header.content_type = nil
         }
     }
 --- request
@@ -1758,6 +1799,26 @@ GET /t
 --- response_body
 foo
 --- response_headers
-Content-Type:
+!Content-Type
+--- no_error_log
+[error]
+
+
+
+=== TEST 80: don't generate default Content-Type when Content-Type is set
+--- config
+    location = /t {
+        default_type text/html;
+        content_by_lua_block {
+            ngx.header["Content-Type"] = "application/json"
+            ngx.say("foo")
+        }
+    }
+--- request
+GET /t
+--- response_body
+foo
+--- response_headers
+Content-Type: application/json
 --- no_error_log
 [error]