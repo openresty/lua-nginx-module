openresty/lua-nginx-module

commit 7c2b58ee7e66689ef176b251d43bfc84f7db26c6

change: we now avoid running init_by_lua* in signaller processes and when testing the nginx configuration.
Signed-off-by: Thibault Charbonnier <thibaultcha@me.com>

t/160-disable-init-by-lua.t
@@ -1,0 +1,131 @@
+use Test::Nginx::Socket::Lua;
+
+repeat_each(2);
+
+plan tests => repeat_each() * (blocks() * 2);
+
+$ENV{TEST_NGINX_HTML_DIR} ||= html_dir();
+
+my $html_dir = $ENV{TEST_NGINX_HTML_DIR};
+my $http_config = <<_EOC_;
+    init_by_lua_block {
+        function set_up_ngx_tmp_conf()
+            local conf = [[
+                events {
+                    worker_connections 64;
+                }
+                http {
+                    init_by_lua_block {
+                        ngx.log(ngx.ERR, "run init_by_lua")
+                    }
+                }
+            ]]
+
+            assert(os.execute("mkdir -p $html_dir/logs"))
+
+            local conf_file = "$html_dir/nginx.conf"
+            local f, err = io.open(conf_file, "w")
+            if not f then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+
+            assert(f:write(conf))
+
+            return conf_file
+        end
+
+        function get_ngx_bin_path()
+            local ffi = require "ffi"
+            ffi.cdef[[char **ngx_argv;]]
+            return ffi.string(ffi.C.ngx_argv[0])
+        end
+    }
+_EOC_
+
+add_block_preprocessor(sub {
+    my $block = shift;
+
+    if (!defined $block->http_config) {
+        $block->set_value("http_config", $http_config);
+    }
+
+    if (!defined $block->request) {
+        $block->set_value("request", "GET /t");
+    }
+});
+
+log_level("warn");
+no_long_string();
+run_tests();
+
+__DATA__
+
+=== TEST 1: ensure init_by_lua* is not run in signaller process
+--- config
+    location = /t {
+        content_by_lua_block {
+            local conf_file = set_up_ngx_tmp_conf()
+            local nginx = get_ngx_bin_path()
+
+            local cmd = nginx .. " -p $TEST_NGINX_HTML_DIR -c " .. conf_file .. " -s reopen"
+            local p, err = io.popen(cmd)
+            if not p then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+
+            local out, err = p:read("*a")
+            if not out then
+                ngx.log(ngx.ERR, err)
+
+            else
+                ngx.log(ngx.WARN, out)
+            end
+        }
+    }
+--- no_error_log eval
+qr/\[error\] .*? init_by_lua:\d+: run init_by_lua/
+
+
+
+=== TEST 2: init_by_lua* does not run when testing Nginx configuration
+--- config
+    location = /t {
+        content_by_lua_block {
+            local conf_file = set_up_ngx_tmp_conf()
+            local nginx = get_ngx_bin_path()
+
+            local cmd = nginx .. " -p $TEST_NGINX_HTML_DIR -c " .. conf_file .. " -t"
+            local p, err = io.popen(cmd)
+            if not p then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+
+            local out, err = p:read("*a")
+            if not out then
+                ngx.log(ngx.ERR, err)
+
+            else
+                ngx.log(ngx.WARN, out)
+            end
+
+            local cmd = nginx .. " -p $TEST_NGINX_HTML_DIR -c " .. conf_file .. " -T"
+            local p, err = io.popen(cmd)
+            if not p then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+
+            local out, err = p:read("*a")
+            if not out then
+                ngx.log(ngx.ERR, err)
+
+            else
+                ngx.log(ngx.WARN, out)
+            end
+        }
+    }
+--- no_error_log eval
+qr/\[error\] .*? init_by_lua:\d+: run init_by_lua/

src/ngx_http_lua_module.c
@@ -647,6 +647,10 @@ ngx_http_lua_init(ngx_conf_t *cf)
 #endif
     ngx_str_t                   name = ngx_string("host");
 
+    if (ngx_process == NGX_PROCESS_SIGNALLER || ngx_test_config) {
+        return NGX_OK;
+    }
+
     lmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_lua_module);
 
     lmcf->host_var_index = ngx_http_get_variable_index(cf, &name);

commit 055bb17c6cf35258f06460d546468c114c591587

bugfix: fixed a segfault introduced in 7c2b58e when testing an nginx configuration containing 'lua_shared_dict' directive(s).
In commit 7c2b58e, we avoided running `init_by_lua*` in signaller processes and
when testing the Nginx configuration. However, when the Nginx configuration
contains `lua_shared_dict` directives, the execution of `init_by_lua*` will be
postponded when testing the configuration.

This case was not handled and led to the segmentation fault reported in #1462.

src/ngx_http_lua_api.c
@@ -195,7 +195,7 @@ ngx_http_lua_shared_memory_init(ngx_shm_zone_t *shm_zone, void *data)
     lmcf->shm_zones_inited++;
 
     if (lmcf->shm_zones_inited == lmcf->shm_zones->nelts
-        && lmcf->init_handler)
+        && lmcf->init_handler && !ngx_test_config)
     {
         saved_cycle = ngx_cycle;
         ngx_cycle = ctx->cycle;

t/160-disable-init-by-lua.t
@@ -9,17 +9,19 @@ $ENV{TEST_NGINX_HTML_DIR} ||= html_dir();
 my $html_dir = $ENV{TEST_NGINX_HTML_DIR};
 my $http_config = <<_EOC_;
     init_by_lua_block {
-        function set_up_ngx_tmp_conf()
-            local conf = [[
-                events {
-                    worker_connections 64;
-                }
-                http {
-                    init_by_lua_block {
-                        ngx.log(ngx.ERR, "run init_by_lua")
+        function set_up_ngx_tmp_conf(conf)
+            if conf == nil then
+                conf = [[
+                    events {
+                        worker_connections 64;
                     }
-                }
-            ]]
+                    http {
+                        init_by_lua_block {
+                            ngx.log(ngx.ERR, "run init_by_lua")
+                        }
+                    }
+                ]]
+            end
 
             assert(os.execute("mkdir -p $html_dir/logs"))
 
@@ -129,3 +131,57 @@ qr/\[error\] .*? init_by_lua:\d+: run init_by_lua/
     }
 --- no_error_log eval
 qr/\[error\] .*? init_by_lua:\d+: run init_by_lua/
+
+
+
+=== TEST 3: init_by_lua* does not run when testing Nginx configuration which contains 'lua_shared_dict' (GitHub #1462)
+--- config
+    location = /t {
+        content_by_lua_block {
+            local conf = [[
+                events {
+                    worker_connections 64;
+                }
+                http {
+                    lua_shared_dict test 64k;
+                    init_by_lua_block {
+                        ngx.log(ngx.ERR, "run init_by_lua with lua_shared_dict")
+                    }
+                }
+            ]]
+            local conf_file = set_up_ngx_tmp_conf(conf)
+            local nginx = get_ngx_bin_path()
+
+            local cmd = nginx .. " -p $TEST_NGINX_HTML_DIR -c " .. conf_file .. " -t"
+            local p, err = io.popen(cmd)
+            if not p then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+
+            local out, err = p:read("*a")
+            if not out then
+                ngx.log(ngx.ERR, err)
+
+            else
+                ngx.log(ngx.WARN, out)
+            end
+
+            local cmd = nginx .. " -p $TEST_NGINX_HTML_DIR -c " .. conf_file .. " -T"
+            local p, err = io.popen(cmd)
+            if not p then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+
+            local out, err = p:read("*a")
+            if not out then
+                ngx.log(ngx.ERR, err)
+
+            else
+                ngx.log(ngx.WARN, out)
+            end
+        }
+    }
+--- no_error_log eval
+qr/\[error\] .*? init_by_lua:\d+: run init_by_lua with lua_shared_dict/