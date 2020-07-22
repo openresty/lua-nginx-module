2c5b1893348d02aa8a2f0e4bbdc7c30798776401
Yan Zhu <yanzhu@alauda.io>
2020-07-22 12:50:40 +0800

    feature: implemented the new ngx.ssl.server_port() API to get server port.

--- src/ngx_http_lua_ssl_certby.c
+++ src/ngx_http_lua_ssl_certby.c
@@ -859,6 +859,39 @@ ngx_http_lua_ffi_ssl_server_name(ngx_http_request_t *r, char **name,
 
 
 int
+ngx_http_lua_ffi_ssl_server_port(ngx_http_request_t *r,
+    unsigned short *server_port, char **err)
+{
+    ngx_ssl_conn_t          *ssl_conn;
+    ngx_connection_t        *c;
+
+    if (r->connection == NULL || r->connection->ssl == NULL) {
+        *err = "bad request";
+        return NGX_ERROR;
+    }
+
+    ssl_conn = r->connection->ssl->connection;
+    if (ssl_conn == NULL) {
+        *err = "bad ssl conn";
+        return NGX_ERROR;
+    }
+
+    c = ngx_ssl_get_connection(ssl_conn);
+
+    switch (c->local_sockaddr->sa_family) {
+
+    case AF_UNIX:
+        *err = "unix domain has no port";
+        return NGX_ERROR;
+
+    default:
+        *server_port = (unsigned short) ngx_inet_get_port(c->local_sockaddr);
+        return NGX_OK;
+    }
+}
+
+
+int
 ngx_http_lua_ffi_ssl_raw_client_addr(ngx_http_request_t *r, char **addr,
     size_t *addrlen, int *addrtype, char **err)
 {
