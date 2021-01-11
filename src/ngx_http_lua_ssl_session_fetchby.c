947dbf059e3af933f81709c013b634e813b33478
Ilya Shipitsin <chipitsine@gmail.com>
2021-01-11 11:57:43 +0500

    fix style

--- src/ngx_http_lua_ssl_session_fetchby.c
+++ src/ngx_http_lua_ssl_session_fetchby.c
@@ -451,7 +451,7 @@ ngx_http_lua_log_ssl_sess_fetch_error(ngx_log_t *log, u_char *buf, size_t len)
 
         if (c->listening && c->listening->addr_text.len) {
             p = ngx_snprintf(buf, len, ", server: %V", \
-            &c->listening->addr_text);
+                             &c->listening->addr_text);
             buf = p;
         }
     }
