f72792234c56e10b9ef565015df034ab55a6cfc1
Johnny Wang <wangjiahao@openresty.com>
2021-05-14 00:37:40 +0800

    doc: stripped line trailing whitespaces. (#1881)
    * stripped the trailing whitespaces introduced in ad2b404d.
    
    * stripped the trailing whitespaces introduced in 88a52c44.

--- src/ngx_http_lua_timer.c
+++ src/ngx_http_lua_timer.c
@@ -741,7 +741,7 @@ ngx_http_lua_log_timer_error(ngx_log_t *log, u_char *buf, size_t len)
         }
 
         if (c->listening && c->listening->addr_text.len) {
-            p = ngx_snprintf(buf, len, ", server: %V", 
+            p = ngx_snprintf(buf, len, ", server: %V",
                              &c->listening->addr_text);
             /* len -= p - buf; */
             buf = p;
