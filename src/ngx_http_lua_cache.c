de61423e97e0ae538b4d00ebb58c01a0bb528b6e
jizhuozhi <jizhuozhi@jd.com>
2022-01-18 23:00:42 +0800

    feature: content_by_lua_file should not return 404 if file permission denied #1992
    Fix code style

--- src/ngx_http_lua_cache.c
+++ src/ngx_http_lua_cache.c
@@ -309,11 +309,16 @@ ngx_http_lua_cache_loadfile(ngx_log_t *log, lua_State *L,
         case LUA_ERRFILE:
             if (errno == ENOENT) {
                 errcode = NGX_HTTP_NOT_FOUND;
-            } else if (errno == EACCES) {
+            }
+
+            else if (errno == EACCES) {
                 errcode = NGX_HTTP_FORBIDDEN;
-            } else {
+            }
+
+            else {
                 errcode = NGX_HTTP_INTERNAL_SERVER_ERROR;
             }
+
             /* fall through */
 
         default:
