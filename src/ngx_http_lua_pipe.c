b6db2e741dc77fa7c5f6da12ef7d7f9c26a930a5
lijunlong <lijunlong@openresty.com>
2022-05-25 21:04:25 +0800

    optimize: add error log when closing the pipe failed. (#2050)

--- src/ngx_http_lua_pipe.c
+++ src/ngx_http_lua_pipe.c
@@ -773,10 +773,21 @@ ngx_http_lua_ffi_pipe_spawn(ngx_http_lua_ffi_pipe_proc_t *proc,
             }
         }
 
-        close(in[0]);
-        close(out[1]);
+        if (close(in[0]) == -1) {
+            ngx_log_error(NGX_LOG_EMERG, ngx_cycle->log, ngx_errno,
+                          "lua pipe failed to close the in[0]");
+        }
+
+        if (close(out[1]) == -1) {
+            ngx_log_error(NGX_LOG_EMERG, ngx_cycle->log, ngx_errno,
+                          "lua pipe failed to close the out[1]");
+        }
+
         if (!merge_stderr) {
-            close(err[1]);
+            if (close(err[1]) == -1) {
+                ngx_log_error(NGX_LOG_EMERG, ngx_cycle->log, ngx_errno,
+                              "lua pipe failed to close the err[1]");
+            }
         }
 
         if (environ != NULL) {
