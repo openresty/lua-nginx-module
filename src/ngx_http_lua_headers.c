1953052a026eeec5dae9af96cc189d8725d72a5e
lijunlong <lijunlong@openresty.com>
2023-03-09 16:32:17 +0800

    bugfix: fixed warning of signed and unsigned.
    Signed-off-by: lijunlong <lijunlong@openresty.com>

--- src/ngx_http_lua_headers.c
+++ src/ngx_http_lua_headers.c
@@ -1250,8 +1250,10 @@ ngx_http_lua_ngx_raw_header_cleanup(void *data)
 int
 ngx_http_lua_ffi_set_resp_header_macos(ngx_http_lua_set_resp_header_params_t *p)
 {
-    return ngx_http_lua_ffi_set_resp_header(p->r, p->key_data, p->key_len,
-                                            p->is_nil, p->sval, p->sval_len,
+    return ngx_http_lua_ffi_set_resp_header(p->r, (const u_char *) p->key_data,
+                                            p->key_len, p->is_nil,
+                                            (const u_char *) p->sval,
+                                            p->sval_len,
                                             p->mvals, p->mvals_len,
                                             p->override, p->errmsg);
 }
