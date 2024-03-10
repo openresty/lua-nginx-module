71cab1935f5d79c766afb334cf065c3faae097b6
lijunlong <lijunlong@openresty.com>
2024-03-10 19:09:43 +0800

    fixed style

--- src/ngx_http_lua_ssl_export_keying_material.c
+++ src/ngx_http_lua_ssl_export_keying_material.c
@@ -23,6 +23,7 @@
 #include "ngx_http_lua_ssl_session_fetchby.h"
 #include "ngx_http_lua_ssl.h"
 #include "ngx_http_lua_directive.h"
+#include "ngx_http_lua_ssl_export_keying_material.h"
 
 
 ngx_int_t
@@ -34,8 +35,8 @@ ngx_http_lua_ffi_ssl_export_keying_material(ngx_http_request_t *r,
     *err = "OpenSSL too old";
     return NGX_ERROR;
 #else
-    ngx_connection_t    *c;
-    ngx_ssl_conn_t      *ssl_conn;
+    ngx_connection_t   *c;
+    ngx_ssl_conn_t     *ssl_conn;
     int                 rc;
 
     c = r->connection;
@@ -46,21 +47,22 @@ ngx_http_lua_ffi_ssl_export_keying_material(ngx_http_request_t *r,
 
     ssl_conn = c->ssl->connection;
     if (ssl_conn == NULL) {
-        *err = "bad ssl conn";
+        *err = "bad ssl connection";
         return NGX_ERROR;
     }
 
-    rc = SSL_export_keying_material(ssl_conn,
-        out, out_size, label, llen, context, ctxlen, use_ctx);
+    rc = SSL_export_keying_material(ssl_conn, out, out_size, label, llen,
+                                    context, ctxlen, use_ctx);
     if (rc == 1) {
         return NGX_OK;
     }
 
-    ngx_ssl_error(NGX_LOG_ALERT, c->log, 0,
-        "SSL_export_keying_material rc: %d, error: %s",
-        rc, ERR_error_string(ERR_get_error(), NULL));
+    ngx_ssl_error(NGX_LOG_INFO, c->log, 0,
+                  "SSL_export_keying_material rc: %d, error: %s",
+                  rc, ERR_error_string(ERR_get_error(), NULL));
 
     *err = "SSL_export_keying_material() failed";
+
     return NGX_ERROR;
 #endif
 }
@@ -75,9 +77,9 @@ ngx_http_lua_ffi_ssl_export_keying_material_early(ngx_http_request_t *r,
     *err = "OpenSSL too old";
     return NGX_ERROR;
 #else
-    ngx_connection_t    *c;
+    int                  rc;
     ngx_ssl_conn_t      *ssl_conn;
-    int                 rc;
+    ngx_connection_t    *c;
 
     c = r->connection;
     if (c == NULL || c->ssl == NULL) {
@@ -87,21 +89,23 @@ ngx_http_lua_ffi_ssl_export_keying_material_early(ngx_http_request_t *r,
 
     ssl_conn = c->ssl->connection;
     if (ssl_conn == NULL) {
-        *err = "bad ssl conn";
+        *err = "bad ssl connection";
         return NGX_ERROR;
     }
 
-    rc = SSL_export_keying_material_early(ssl_conn,
-        out, out_size, label, llen, context, ctxlen);
+    rc = SSL_export_keying_material_early(ssl_conn, out, out_size,
+                                          label, llen, context, ctxlen);
+
     if (rc == 1) {
         return NGX_OK;
     }
 
-    ngx_ssl_error(NGX_LOG_ALERT, c->log, 0,
-        "SSL_export_keying_material_early rc: %d, error: %s",
-        rc, ERR_error_string(ERR_get_error(), NULL));
+    ngx_ssl_error(NGX_LOG_INFO, c->log, 0,
+                  "SSL_export_keying_material_early rc: %d, error: %s",
+                  rc, ERR_error_string(ERR_get_error(), NULL));
 
     *err = "SSL_export_keying_material_early() failed";
+
     return NGX_ERROR;
 #endif
 }
