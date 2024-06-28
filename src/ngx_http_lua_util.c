892b7ee5e84e7e24e5976de45e6fdfdbc08233e2
Jun Ouyang <ouyangjun1999@gmail.com>
2024-06-28 08:46:45 +0800

    bugfix: lua-nginx-module context was clear when ngx.send_header() trigger filter_finalize case.

--- src/ngx_http_lua_util.c
+++ src/ngx_http_lua_util.c
@@ -549,6 +549,10 @@ ngx_http_lua_send_header_if_needed(ngx_http_request_t *r,
         if (!ctx->buffering) {
             dd("sending headers");
             rc = ngx_http_send_header(r);
+            if (r->filter_finalize) {
+                ngx_http_set_ctx(r, ctx, ngx_http_lua_module);
+            }
+
             ctx->header_sent = 1;
             return rc;
         }
