openresty/lua-nginx-module

commit de1b896999c95bcbfc2b60c4a2247092d9c7fe40

style: remove duplicate check in ngx.send_headers

src/ngx_http_lua_util.c
@@ -535,13 +535,10 @@ ngx_http_lua_send_header_if_needed(ngx_http_request_t *r,
             r->headers_out.status = NGX_HTTP_OK;
         }
 
-        if (!ctx->mime_set
-            && ngx_http_lua_set_content_type(r, ctx) != NGX_OK)
-        {
-            return NGX_ERROR;
-        }
-
         if (!ctx->headers_set) {
+            if (ngx_http_lua_set_content_type(r) != NGX_OK) {
+                return NGX_ERROR;
+            }
             ngx_http_clear_content_length(r);
             ngx_http_clear_accept_ranges(r);
         }

src/ngx_http_lua_output.c
@@ -722,16 +722,14 @@ ngx_http_lua_ngx_send_headers(lua_State *L)
                                | NGX_HTTP_LUA_CONTEXT_ACCESS
                                | NGX_HTTP_LUA_CONTEXT_CONTENT);
 
-    if (!r->header_sent && !ctx->header_sent) {
-        ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                       "lua send headers");
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+                    "lua send headers");
 
-        rc = ngx_http_lua_send_header_if_needed(r, ctx);
-        if (rc == NGX_ERROR || rc > NGX_OK) {
-            lua_pushnil(L);
-            lua_pushliteral(L, "nginx output filter error");
-            return 2;
-        }
+    rc = ngx_http_lua_send_header_if_needed(r, ctx);
+    if (rc == NGX_ERROR || rc > NGX_OK) {
+        lua_pushnil(L);
+        lua_pushliteral(L, "nginx output filter error");
+        return 2;
     }
 
     lua_pushinteger(L, 1);

commit a634f1028af51ee3c70e94ba8f5176d7f781b3dd

bugfix: build failure introduced by commit de1b896999c95.

src/ngx_http_lua_util.c
@@ -536,7 +536,7 @@ ngx_http_lua_send_header_if_needed(ngx_http_request_t *r,
         }
 
         if (!ctx->headers_set) {
-            if (ngx_http_lua_set_content_type(r) != NGX_OK) {
+            if (ngx_http_lua_set_content_type(r, ctx) != NGX_OK) {
                 return NGX_ERROR;
             }
 