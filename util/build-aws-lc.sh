a0e14c38bd2cc69c095adf67d7ae956486965e39
耗子 <i@haozi.net>
2024-10-09 15:50:14 +0800

    feat: optimize

--- util/build-aws-lc.sh
+++ util/build-aws-lc.sh
@@ -8,7 +8,7 @@
 
 root=`pwd`
 
-tar -xzf aws-lc-1.34.2.tar.gz
-mv aws-lc-1.34.2 aws-lc
+tar -xzf aws-lc.tar.gz
+mv aws-lc-* aws-lc
 cmake $root/aws-lc -GNinja -B$root/aws-lc-build -DCMAKE_INSTALL_PREFIX=/opt/ssl -DBUILD_TESTING=OFF -DDISABLE_GO=ON -DBUILD_TOOL=OFF -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0
 ninja -C $root/aws-lc-build install
