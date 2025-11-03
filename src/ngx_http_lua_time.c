de02114acc8bbd4b17da933d513f736b7f45728b
Sergey A. Osokin <osa@FreeBSD.org>
2025-11-02 21:46:46 -0500

    optimize: add compatibility for freenginx.
    Commit freenginx/nginx@3329aa9 updated the ngx_http_request_t
    structure with the start_time; the start_sec and start_msec
    were removed.

--- src/ngx_http_lua_time.c
+++ src/ngx_http_lua_time.c
@@ -28,7 +28,21 @@ ngx_http_lua_ffi_now(void)
 double
 ngx_http_lua_ffi_req_start_time(ngx_http_request_t *r)
 {
+#if (defined freenginx && nginx_version >= 1029000)
+    ngx_time_t              *tp;
+
+    tp = ngx_timeofday();
+    tp->sec -= (ngx_current_msec - r->start_time) / 1000;
+    tp->msec -= (ngx_current_msec - r->start_time) % 1000;
+    if (tp->msec > NGX_MAX_INT_T_VALUE) {
+        tp->msec += 1000;
+        tp->sec -= 1;
+    }
+
+    return tp->sec + tp->msec / 1000.0;
+#else
     return r->start_sec + r->start_msec / 1000.0;
+#endif
 }
 
 
