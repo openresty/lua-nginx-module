db0fce0179c70c25ef155b0a6f11e7641e1f15cb
Jun Ouyang <ouyangjun1999@gmail.com>
2026-03-10 13:40:48 +0800

    fix

--- src/ngx_http_lua_util.c
+++ src/ngx_http_lua_util.c
@@ -1467,7 +1467,7 @@ user_co_done:
                 }
 
                 if (ctx->cur_co_ctx->is_uthread
-                    && ctx->cur_co_ctx->co_ref != LUA_NOREF)
+                    && ctx->cur_co_ctx->co_ref != LUA_NOREF) {
                     ngx_http_lua_del_thread(r, L, ctx, ctx->cur_co_ctx);
                     ctx->uthreads--;
                 }
